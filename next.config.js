cd76aaf41366af036cefb0fa414fd6e880a3eb27
Tobias <tobias@fixmycity.de>
2022-12-23 10:58:18 +0100

    Next config: Enable source map on production
    … because we are here to share :)

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const { withBlitz } = require("@blitzjs/next")
 const config = {
   reactStrictMode: true,
   swcMinify: true,
+  productionBrowserSourceMaps: true, // build source maps in production – https://nextjs.org/docs/advanced-features/source-maps
 }
 
 module.exports = withBlitz(config)
