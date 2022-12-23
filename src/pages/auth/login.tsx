2804ba2fd33c5a10a6c61e62b0fc5cbfab928a2b
Tobias <tobias@fixmycity.de>
2022-12-23 10:36:11 +0100

    DEBUG: Adding a notice on login causes server error 500
    The goal is, to show a notice which informs the user, that she is logged in already and provides a logout action.
    
    However, the `useCurrentUser` part will throw a error 500 on the server. I did not manage to get debugging info going to see why this error is there…

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -5,10 +5,26 @@ import { Layout, MetaTags } from "src/core/layouts"
 
 const LoginPage: BlitzPage = () => {
   const router = useRouter()
+  // const currentUser = useCurrentUser()
+  // const [logoutMutation] = useMutation(logout)
+  // const handleLogout = async () => {
+  //   await logoutMutation()
+  // }
 
   return (
     <Layout>
       <MetaTags noindex title="Anmelden" />
+      {/* {currentUser && (
+        <Notice
+          type="info"
+          title="Du bist bereits eingeloggt"
+          actionText="Jetzt ausloggen…"
+          action={handleLogout}
+        >
+          Du bist bereits eingeloggt. Wenn du den Account wechseln möchtest, musst du dich zuerst
+          ausloggen.
+        </Notice>
+      )} */}
       <LoginForm
         onSuccess={(_user) => {
           const next = router.query.next ? decodeURIComponent(router.query.next as string) : "/"
