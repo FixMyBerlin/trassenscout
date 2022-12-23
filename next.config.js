f789e62680fcb901325623b8d969a47c2ec591b0
Tobias <tobias@fixmycity.de>
2022-12-23 10:58:35 +0100

    Next config: Placeholder to secure image fetching

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const config = {
   reactStrictMode: true,
   swcMinify: true,
   productionBrowserSourceMaps: true, // build source maps in production – https://nextjs.org/docs/advanced-features/source-maps
+  images: {
+    domains: [], // allow fetching images from those domains – https://nextjs.org/docs/api-reference/next/image#domains
+  },
 }
 
 module.exports = withBlitz(config)
