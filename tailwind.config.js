d2924e00e12a3e1b8833fa8e15c61f8e367986d0
Tobias <tobias@fixmycity.de>
2023-01-04 15:59:22 +0100

    Migrate tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "rsv-pink": "#e6007d",
+        "rsv-blau": "#0f65af",
+        "rsv-ochre": "#eab308",
+      },
       typography: {
         DEFAULT: {
           // Tell the plugin to not add any classes to anchor tags
@@ -10,6 +15,10 @@ module.exports = {
           css: { a: false },
         },
       },
+    },
+    fontFamily: {
+      sans: ["OverpassVariable", "sans-serif"],
+      mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
     },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
