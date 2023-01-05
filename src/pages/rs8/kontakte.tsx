aec6081422057fb258f4a5623c58db6fedc73a0b
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-01-05 12:35:20 +0100

    Update page kontakte

--- src/pages/rs8/kontakte.tsx
+++ src/pages/rs8/kontakte.tsx
@@ -1,11 +1,12 @@
 import { BlitzPage } from "@blitzjs/next"
 import { LayoutRs8, MetaTags } from "src/core/layouts"
+import { PageKontakte } from "src/rs8/kontakte/components"
 
 const Rs8Kontakte: BlitzPage = () => {
   return (
     <LayoutRs8>
       <MetaTags title="RSV Startseite" />
-      Welcome Home.
+      <PageKontakte />
     </LayoutRs8>
   )
 }
