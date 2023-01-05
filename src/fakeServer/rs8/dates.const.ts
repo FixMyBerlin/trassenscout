5672de314012e698a960787bd151d6226650f339
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-01-05 12:34:53 +0100

    Add fakeserver

---
+++ src/fakeServer/rs8/dates.const.ts
@@ -0,0 +1,90 @@
+export type TDate = {
+  title: string;
+  start: string;
+  locationName?: string;
+  locationUrl?: string;
+  description?: string;
+};
+
+export const dates: TDate[] = [
+  {
+    title: 'Koordinations-Termin Abschnitt 2',
+    start: '2023-12-14 10:00',
+    locationName: 'Konferenz Raum A',
+    locationUrl: 'https://us02web.zoom.us/j/2194140224?pwd=123',
+    description:
+      '# Meeting: with the team\n ## Weekly meeting of stakeholders \n I think Ich schreibe hier einen Paragraph. \n First Line discussing *RS 8 - Radschnellverbindung*  Second Line. Lorem **Ipsum** __Lorem__ \n 1. Task \n 2. Task2',
+  },
+  {
+    title: 'Koordinations-Termin Abschnitt 5',
+    start: '2023-10-14 10:30',
+    locationName: 'Konferenz Raum A',
+    locationUrl: 'https://us02web.zoom.us/j/2194140224?pwd=123',
+    description:
+      '# Meeting with the team\n ## Weekly meeting of stakeholders \n Bitte achten Sie auf unser [Digitales Whiteboard](https://placekitten.com/g/200/300)!  \n\n  I think Ich schreibe hier einen Paragraph. \n First Line discussing *RS 8 - Radschnellverbindung*  Second Line. Lorem **Ipsum** __Lorem__ \n 1. Task \n 2. Task2',
+  },
+  {
+    title: 'Abstimmung Kreuzungspunkt 12',
+    start: '2023-02-14 10:00',
+    locationName: 'Hauptstraße Ecke Bergstraße',
+    locationUrl: undefined,
+    description: undefined,
+  },
+  {
+    title: 'Company holiday party',
+    start: '2021-12-15 07:00',
+    locationName: undefined,
+    locationUrl:
+      'https://us02web.zoom.us/j/219414kjshfgsahfdglsjdfgjhsfgsjfdhfg0224?pwd=123',
+    description:
+      'Wir verwenden ein. \nDieser Link ist ohne Markdown-Link-Tags: https://placekitten.com/g/200/300 . \nCelebrate the end of the year with food, look here: https://google.de, drinks, and music.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+  },
+  {
+    title: 'Business strategy planning',
+    start: '2023-12-16 09:00',
+    locationName: undefined,
+    locationUrl: 'https://us02web.zoom.us/j/2194140224?pwd=123',
+    description:
+      '## TO: \n - Task 1: https://miro.com \n - Task 2 \n - Task 3  \n   \n Develop plans for the upcoming year and set goals.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+  },
+  {
+    title: 'Client presentation',
+    start: '2022-12-17 11:00',
+    locationName: 'Office',
+    locationUrl: undefined,
+    description:
+      'Present our latest product to potential clients. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+  },
+  {
+    title: 'Team building event',
+    start: '2022-12-18 01:00',
+    locationName: 'Tempelhofer Feld',
+    locationUrl: undefined,
+    description:
+      'Participate in team-building activities and bonding exercises',
+  },
+  {
+    title: 'Business strategy planning',
+    start: '2022-12-22 09:00',
+    locationName: undefined,
+    locationUrl: 'https://us02web.zoom.us/j/2194140224?pwd=123',
+    description:
+      'Develop plans for the upcoming year and set goals.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+  },
+  {
+    title: 'Client presentation',
+    start: '2022-12-22 11:00',
+    locationName: 'Office',
+    locationUrl: undefined,
+    description:
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+  },
+  {
+    title: 'Team building event',
+    start: '2022-12-23 01:00',
+    locationName: 'Tempelhofer Feld',
+    locationUrl: undefined,
+    description:
+      'Participate in team-building activities and bonding exercises',
+  },
+];
