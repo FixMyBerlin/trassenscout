e93cbc1f56a53bbafce56d8f0023cd080aa3879a
Tobias <tobias@fixmycity.de>
2023-01-05 14:45:43 +0100

    Seed: Add dev user, prepare seed for calendarEntries

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,4 +1,4 @@
-// import db from "./index"
+import db from "./index"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -7,8 +7,45 @@
  * to easily generate realistic data.
  */
 const seed = async () => {
+  const user = await db.user.create({
+    data: {
+      email: "dev-team@fixmycity.de",
+      // password: dev-team@fixmycity.de
+      hashedPassword:
+        "JGFyZ29uMmlkJHY9MTkkbT02NTUzNix0PTIscD0xJDRMWm82dmVrRk91VnVlZTVwcEpiS3ckOHFZcHhyM2RITm0yTGxTeXdqeEcxSWFsZEJCUWhxNVZxdm53eHoxTk4xTQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+    },
+  })
+
+  // {
+  //   title: 'Koordinations-Termin Abschnitt 2',
+  //   start: '2023-12-14 10:00',
+  //   locationName: 'Konferenz Raum A',
+  //   locationUrl: 'https://us02web.zoom.us/j/2194140224?pwd=123',
+  //   description:
+  //     '# Meeting: with the team\n ## Weekly meeting of stakeholders \n I think Ich schreibe hier einen Paragraph. \n First Line discussing *RS 8 - Radschnellverbindung*  Second Line. Lorem **Ipsum** __Lorem__ \n 1. Task \n 2. Task2',
+  // },
+  // {
+  //   title: 'Koordinations-Termin Abschnitt 5',
+  //   start: '2023-10-14 10:30',
+  //   locationName: 'Konferenz Raum A',
+  //   locationUrl: 'https://us02web.zoom.us/j/2194140224?pwd=123',
+  //   description:
+  //     '# Meeting with the team\n ## Weekly meeting of stakeholders \n Bitte achten Sie auf unser [Digitales Whiteboard](https://placekitten.com/g/200/300)!  \n\n  I think Ich schreibe hier einen Paragraph. \n First Line discussing *RS 8 - Radschnellverbindung*  Second Line. Lorem **Ipsum** __Lorem__ \n 1. Task \n 2. Task2',
+  // },
+  // {
+  //   title: 'Abstimmung Kreuzungspunkt 12',
+  //   start: '2023-02-14 10:00',
+  //   locationName: 'Hauptstraße Ecke Bergstraße',
+  //   locationUrl: undefined,
+  //   description: undefined,
+  // },
+
   // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
+  //   await db.date.create({
+  //     data: {
+  //       title: `Date: ${i}`,
+  //     },
+  //   })
   // }
 }
 
