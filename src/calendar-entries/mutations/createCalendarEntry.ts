0c3fe7f4e21df899f506fe3c8e157e7173f14d13
Sven König <sven@fixmycity.de>
2023-01-06 13:34:50 +0100

    calendarEntry: use new zod datetime()

--- src/calendar-entries/mutations/createCalendarEntry.ts
+++ src/calendar-entries/mutations/createCalendarEntry.ts
@@ -3,22 +3,24 @@ import db from "db"
 import { z } from "zod"
 
 const CreateCalendarEntry = z.object({
-  title: z.string(),
-  startAt: z.string(), // TODO .datetime()
+  title: z.string().min(8),
+  startAt: z.string().datetime(),
   locationName: z.string().nullish(),
   locationUrl: z.string().nullish(),
   description: z.string().nullish(),
 })
 
 export default resolver.pipe(
+  (input: { startAt: string }) => {
+    // value will be formatted YYYY-MM-DDThh:mm when using <input type="datetime-local">
+    // see: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/datetime-local#value
+    input.startAt += ":00Z"
+    return input
+  },
   resolver.zod(CreateCalendarEntry),
   resolver.authorize(),
   async (input) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const startAt = new Date(input.startAt)
-
-    const calendarEntry = await db.calendarEntry.create({ data: { ...input, startAt } })
-
-    return calendarEntry
+    return await db.calendarEntry.create({ data: input })
   }
 )
