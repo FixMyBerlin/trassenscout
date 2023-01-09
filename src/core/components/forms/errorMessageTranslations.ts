cd9675e4da25460ab14b61ddf423f1d53942319e
Johanna Michel <76495099+JohannaPeanut@users.noreply.github.com>
2023-01-09 17:31:13 +0100

    Update src/core/components/forms/errorMessageTranslations.ts
    Co-authored-by: Tobias <tobias@fixmycity.de>

--- src/core/components/forms/errorMessageTranslations.ts
+++ src/core/components/forms/errorMessageTranslations.ts
@@ -3,6 +3,5 @@ type TranslatedMessages = { [key: string]: string }
 export const errorMessageTranslations: TranslatedMessages = {
   "Error: Invalid `prisma.contact.create()` invocation:Unique constraint failed on the fields: (`email`)":
     "Diese E-Mail-Adresse wird bereits vergeben. Eine E-Mail-Adresse darf nur einem Kontakt zugewiesen werden.",
-  'ZodError: [  {    "validation": "email",    "code": "invalid_string",    "message": "Invalid email",    "path": [      "email"    ]  }]':
     "Dies ist keine gültige E-Mail-Adresse.",
 }
