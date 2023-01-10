a4dea2e35676c046fc38ce29be77b9968e09c1ec
Tobias <tobias@fixmycity.de>
2023-01-10 15:37:39 +0100

    CalendarEntry: Fix zod Schema for URL
    Now, URL is optional but validated if present.

--- src/calendar-entries/schema.ts
+++ src/calendar-entries/schema.ts
@@ -4,7 +4,8 @@ export const CalendarEntrySchema = z.object({
   title: z.string().min(3, { message: "Pflichtfeld. Mindestens 3 Zeichen." }),
   startAt: z.string().datetime({ message: "Kein gültiges Datumsformat." }),
   locationName: z.string().nullish(),
-  locationUrl: z.string().url({ message: "Kein gültige URL." }).nullish(),
+  // Either .nullish, or .url
+  locationUrl: z.union([z.string().nullish(), z.string().url({ message: "Kein gültige URL." })]),
   description: z.string().nullish(),
 })
 
