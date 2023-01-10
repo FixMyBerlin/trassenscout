fcbac247ce293935098423c3addad0caada54018
Tobias <tobias@fixmycity.de>
2023-01-10 16:46:53 +0100

    CalendarEntry: Fix zod Schema for URL – take 2

--- src/calendar-entries/schema.ts
+++ src/calendar-entries/schema.ts
@@ -4,8 +4,13 @@ export const CalendarEntrySchema = z.object({
   title: z.string().min(3, { message: "Pflichtfeld. Mindestens 3 Zeichen." }),
   startAt: z.string().datetime({ message: "Kein gültiges Datumsformat." }),
   locationName: z.string().nullish(),
-  // Either .nullish, or .url
-  locationUrl: z.union([z.string().nullish(), z.string().url({ message: "Kein gültige URL." })]),
+  // Either emtpy, or url (https://github.com/colinhacks/zod/issues/798#issuecomment-990891815)
+  locationUrl: z.union([
+    z.undefined(),
+    z.null(),
+    z.literal(""),
+    z.string().url({ message: "Kein gültige URL." }),
+  ]),
   description: z.string().nullish(),
 })
 
