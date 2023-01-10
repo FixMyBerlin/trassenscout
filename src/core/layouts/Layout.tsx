37bf90f5140aff3614916ae1dd9ce78971ed4bf0
Tobias <tobias@fixmycity.de>
2023-01-10 21:27:05 +0100

    LayoutMiddleBox: Hide primary navigation

--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -3,10 +3,11 @@ import Head from "next/head"
 import React from "react"
 
 type Props = {
+  navigation?: boolean
   children?: React.ReactNode
 }
 
-export const Layout: BlitzLayout<Props> = ({ children }) => {
+export const Layout: BlitzLayout<Props> = ({ navigation, children }) => {
   return (
     <>
       <Head>
@@ -14,7 +15,9 @@ export const Layout: BlitzLayout<Props> = ({ children }) => {
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <div className="text-dark-gray relative flex h-full flex-col overflow-x-hidden">
-        <nav className="h-10 w-full bg-gray-800 text-white">Navigation TODO</nav>
+        {navigation !== false && (
+          <nav className="h-10 w-full bg-gray-800 text-white">Navigation TODO</nav>
+        )}
         <main className="mx-auto w-full max-w-7xl px-6 pb-16 md:px-8">{children}</main>
       </div>
     </>
