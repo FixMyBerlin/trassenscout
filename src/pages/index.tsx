10f3d0791ccea9608514613978708c6d4adc5a76
Tobias <tobias@fixmycity.de>
2023-01-11 17:47:25 +0100

    Homepage: Add linkt to RS8

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
-import { BlitzPage } from "@blitzjs/next"
+import { BlitzPage, Routes } from "@blitzjs/next"
+import { Link } from "src/core/components/links"
 import { Layout, MetaTags } from "src/core/layouts"
 
 const Home: BlitzPage = () => {
   return (
     <Layout>
       <MetaTags title="RSV Startseite" />
-      Welcome Home.
+      Startseite Logged Out UND Startseite Logged In
+      <ul>
+        <li>
+          <Link href={Routes.Rs8Index()}>RS8</Link>
+        </li>
+      </ul>
     </Layout>
   )
 }
