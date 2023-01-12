2e992d3f05101ecd0c497cf412573ffd31bff68f
Tobias <tobias@fixmycity.de>
2023-01-12 17:14:04 +0100

    Header: Change "Logo" to EyeDropperIcon

--- src/core/layouts/Header/HeaderApp/HeaderAppLogo.tsx
+++ src/core/layouts/Header/HeaderApp/HeaderAppLogo.tsx
@@ -1,12 +1,10 @@
-import { ChatBubbleBottomCenterIcon, ChatBubbleOvalLeftIcon } from "@heroicons/react/20/solid"
-import { MapIcon } from "@heroicons/react/24/outline"
-import React from "react"
+import { EyeDropperIcon } from "@heroicons/react/20/solid"
 
 export const HeaderAppLogo: React.FC = () => {
   return (
     <>
-      <ChatBubbleOvalLeftIcon className="block h-8 w-auto text-yellow-400 lg:hidden" />
-      <ChatBubbleOvalLeftIcon className="hidden h-8 w-auto text-yellow-400 lg:block" />{" "}
+      <EyeDropperIcon className="block h-8 w-auto text-yellow-400 lg:hidden" />
+      <EyeDropperIcon className="hidden h-8 w-auto text-yellow-400 lg:block" />{" "}
       <span className="ml-2 text-gray-400">TrassenScout - Abstimmungsplattform</span>
     </>
   )
