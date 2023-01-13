3b3e6ac1d4a36817273c8aa826016f565b8691f5
Tobias <tobias@fixmycity.de>
2023-01-13 06:20:52 +0100

    CalendarEntries: Fix error zod message in `coerce` case
    Thanks at https://github.com/colinhacks/zod/discussions/1851#discussioncomment-4649675

--- src/calendar-entries/schema.ts
+++ src/calendar-entries/schema.ts
@@ -3,9 +3,13 @@ import { z } from "zod"
 export const CalendarEntrySchema = z.object({
   title: z.string().min(3, { message: "Pflichtfeld. Mindestens 3 Zeichen." }),
   startAt: z.coerce.date({
-    // TODO `coerce` makes it, that the error messages on date() don't show becaue zod returns the error given by `new Date(input)`.
-    required_error: "Datum ist erforderlich.",
-    invalid_type_error: "Kein gültiges Datumsformat.",
+    // `coerce` makes it that we need to work around a nontranslatable error
+    // Thanks to https://github.com/colinhacks/zod/discussions/1851#discussioncomment-4649675
+    errorMap: ({ code }, { defaultError }) => {
+      if (code == "invalid_date")
+        return { message: "Pflichfeld. Das Datum ist nicht richtig formatiert." }
+      return { message: defaultError }
+    },
   }),
   locationName: z.string().nullish(),
   // Either emtpy, or url (https://github.com/colinhacks/zod/pull/1849)
