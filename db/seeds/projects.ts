ac658fa78afcb24aa7c8f37f69f1c1028e342235
Tobias <tobias@fixmycity.de>
2023-01-18 15:34:10 +0100

    Seeds: "RS Spree"
    A nice face RS for Berlin with to sections and multiple subsections each.
    
    And some empty data to test the UI when parts of the whole are not created yet.

--- db/seeds/projects.ts
+++ db/seeds/projects.ts
@@ -3,17 +3,31 @@ import db, { Project } from "../index"
 const seedProjects = async () => {
   const seeData: Omit<Project, "id" | "createdAt" | "updatedAt">[] = [
     {
-      slug: "rs3000",
-      name: "Radschnellverbindung 3000",
-      shortName: "RS3k",
+      slug: "rs-spree",
+      name: "Radschnellverbindung Berliner Wasserwege",
+      shortName: "RS Spree",
       description: `*Lorem ipsum dolor sit amet*, consetetur sadipscing elitr.
 
 Sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr.`,
       managerId: 1,
     },
     {
-      slug: "rs3001",
-      name: "Radschnellverbindung 3001",
+      slug: "rs3000",
+      name: "Radschnellverbindung 3000",
+      shortName: "RS 3000",
+      description: null,
+      managerId: 2,
+    },
+    {
+      slug: "rs-blank-section",
+      name: "Radschnellverbindung No Section",
+      shortName: null,
+      description: null,
+      managerId: 2,
+    },
+    {
+      slug: "rs-blank-subsection",
+      name: "Radschnellverbindung No Subsection",
       shortName: null,
       description: null,
       managerId: 2,
--- db/seeds/sections.ts
+++ db/seeds/sections.ts
@@ -2,9 +2,10 @@ import db, { Section } from "../index"
 
 const seedSections = async () => {
   const seeData: Omit<Section, "id" | "createdAt" | "updatedAt">[] = [
+    // Project 1:
     {
       slug: "teilstrecke-1",
-      name: "Section 1",
+      name: "Teilstrecke 1: Spree",
       index: 1,
       description: `*Lorem ipsum dolor sit amet*, consetetur sadipscing elitr.
 
@@ -16,11 +17,31 @@ Sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
     },
     {
       slug: "teilstrecke-2",
-      name: "Section 2",
+      name: "Teilstrecke 2: Kanal",
       index: 2,
       description: null,
       managerId: 1,
       projectId: 1,
+    },
+    // Project 2:
+    {
+      slug: "teilstrecke-1",
+      name: "Teilstrecke 1",
+      index: 2,
+      description: null,
+      managerId: 1,
+      projectId: 2,
+    },
+    // Project 4:
+    // Blank
+    // Project 4:
+    {
+      slug: "teilstrecke-1",
+      name: "Teilstrecke 1",
+      index: 2,
+      description: null,
+      managerId: 1,
+      projectId: 4,
     },
   ]
 
