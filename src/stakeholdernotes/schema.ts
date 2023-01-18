95d9210dd83a7ad82cf580e44102c838a99fc5c6
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-01-18 14:08:15 +0100

    add zod schema

---
+++ src/stakeholdernotes/schema.ts
@@ -0,0 +1,7 @@
+import { z } from "zod"
+
+export const StakeholdernoteSchema = z.object({
+  title: z.string().min(2, { message: "Pflichtfeld. Mindestens 2 Zeichen." }),
+  statusText: z.string().nullish(),
+  status: z.enum(["irrelevant", "pending", "inprogress", "done"]),
+})
