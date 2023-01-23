695032f6db69fdad01d97ffceeaf9bb31c6651ac
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-01-23 12:19:25 +0100

    fix schema subsection

--- src/subsections/schema.ts
+++ src/subsections/schema.ts
@@ -3,13 +3,13 @@ import { z } from "zod"
 
 export const SubsectionSchema = z.object({
   slug: SlugSchema,
-  name: z.string(),
+  title: z.string(),
   description: z.string().nullish(),
   geometry: z.coerce.string().min(20, {
     message: "Pflichtfeld. Format muss ein LineString sein [[9.1943,48.8932],[9.2043,48.8933]].",
   }),
   managerId: z.coerce.number(),
-  sectionId: z.number(),
+  sectionId: z.coerce.number(),
 })
 
 export type TSubsectionSchema = z.infer<typeof SubsectionSchema>
