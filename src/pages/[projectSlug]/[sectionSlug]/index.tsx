ad5a59bb2892d90156813d886bfab7434115b36a
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-01-23 17:16:16 +0100

    update content and style Section page

--- src/pages/[projectSlug]/[sectionSlug]/index.tsx
+++ src/pages/[projectSlug]/[sectionSlug]/index.tsx
@@ -7,13 +7,15 @@ import { Markdown } from "src/core/components/Markdown/Markdown"
 import { PageHeader } from "src/core/components/PageHeader"
 import { Spinner } from "src/core/components/Spinner"
 import { quote } from "src/core/components/text"
+import { H2 } from "src/core/components/text/Headings"
 import { LayoutRs, MetaTags } from "src/core/layouts"
 import { FileTable } from "src/files/components/FileTable"
 import getFiles from "src/files/queries/getFiles"
+import { BaseMap, BaseMapSections, SectionsMap } from "src/projects/components/Map"
 import getProject from "src/projects/queries/getProject"
 import getSection from "src/sections/queries/getSection"
+import getSections from "src/sections/queries/getSections"
 import StakeholdernoteList from "src/stakeholdernotes/components/StakeholdernoteList"
-import getStakeholdernote from "src/stakeholdernotes/queries/getStakeholdernote"
 import getStakeholdernotes from "src/stakeholdernotes/queries/getStakeholdernotes"
 import getSubsections from "src/subsections/queries/getSubsections"
 
@@ -31,37 +33,68 @@ export const SectionDashboardWithQuery = () => {
     where: { section: { slug: sectionSlug! } },
     orderBy: { title: "asc" },
   })
+  const [{ sections }] = useQuery(getSections, {
+    where: { project: { slug: projectSlug! } },
+    orderBy: { id: "asc" },
+    include: { subsections: { select: { id: true, geometry: true } } },
+  })
 
   return (
     <>
       <MetaTags noindex title={section.title} />
+
       <PageHeader title={section.title} subtitle={section.subTitle} />
 
-      <h2>Alle Daten zu unserer Teilstrecke</h2>
+      <div className="mb-12">
+        <p>
+          <strong>Stakeholder:</strong> Todo Status Stakeholder
+        </p>
+        {section.description && (
+          <p>
+            <strong>Kurzinfo:</strong>
+            <Markdown markdown={section.description} />
+          </p>
+        )}
+      </div>
+
+      <div className="mb-12 h-96 w-full sm:h-[500px]">
+        <BaseMap
+          sections={sections as BaseMapSections}
+          selectedSection={section}
+          isInteractive={false}
+        />
+      </div>
+      {/*
       <SuperAdminBox>
         <pre>{JSON.stringify(section, null, 2)}</pre>
-      </SuperAdminBox>
+      </SuperAdminBox> */}
 
-      <h2>Alle {count} Abschnitte diese Teilstrecke</h2>
-      <ul>
-        {subsections.map((subsection) => {
-          const debugSubsection = subsection
-          debugSubsection.geometry = "Gekürzt für die Lesbarkeit"
-          return (
-            <li key={subsection.id}>
-              <strong>{subsection.title}</strong>
-              <Markdown markdown={subsection.description} />
-              <pre>{JSON.stringify(subsection, undefined, 2)}</pre>
-            </li>
-          )
-        })}
-      </ul>
+      <div className="mb-12">
+        <H2 className="mb-5">Alle {count} Abschnitte dieser Teilstrecke</H2>
+        <ul>
+          {subsections.map((subsection) => {
+            const debugSubsection = subsection
+            debugSubsection.geometry = "Gekürzt für die Lesbarkeit"
+            return (
+              <li key={subsection.id}>
+                <strong>{subsection.title}</strong>
+                <Markdown markdown={subsection.description} />
+                <pre>{JSON.stringify(subsection, undefined, 2)}</pre>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
 
-      <h3 className="mb-8 text-2xl font-bold">Wichtige Dateien</h3>
-      <FileTable files={files} />
+      <div className="mb-12">
+        <H2 className="mb-5 text-2xl font-bold">Wichtige Dateien</H2>
+        <FileTable files={files} />
+      </div>
 
-      <h3 className="mb-10 text-2xl font-bold">Stakeholderliste und Status der Abstimmung</h3>
-      <StakeholdernoteList stakeholdernotes={stakeholdernotes} />
+      <div className="mb-12">
+        <H2 className="mb-5 text-2xl font-bold">Stakeholderliste und Status der Abstimmung</H2>
+        <StakeholdernoteList stakeholdernotes={stakeholdernotes} />
+      </div>
 
       <section className="rounded border border-cyan-800 bg-cyan-100 p-5">
         <Link
