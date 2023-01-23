63bd54a2a3cba3c7ba1a613aad5acdf4a29e2adc
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-01-23 17:15:03 +0100

    add Heading components

---
+++ src/core/components/text/Headings.tsx
@@ -0,0 +1,17 @@
+import clsx from "clsx"
+import { ReactNode } from "react"
+
+type Props = {
+  children: ReactNode
+  className?: string
+}
+
+export const H1: React.FC<Props> = ({ className, children }) => {
+  return <h1 className={clsx("text-6xl font-extrabold leading-10", className)}>{children}</h1>
+}
+
+export const H2: React.FC<Props> = ({ className, children }) => {
+  return (
+    <h2 className={clsx("text-dark-gray text-[32px] font-extrabold", className)}>{children}</h2>
+  )
+}
