81d0b75a246a55f75a407145f243ddfa86f1f7bd
Tobias <tobias@fixmycity.de>
2023-02-02 09:41:00 +0100

    Improve HTML+Styles for SectionTeaser Link-Button

--- src/projects/components/Map/SectionsTeaser/SectionsTeaser.tsx
+++ src/projects/components/Map/SectionsTeaser/SectionsTeaser.tsx
@@ -13,19 +13,15 @@ export const SectionsTeaser: React.FC<Props> = ({ section }) => {
   const projectSlug = useParam("projectSlug", "string")
 
   return (
-    <div className="group rounded-md border border-gray-200 bg-white p-5 shadow-md hover:bg-gray-100">
-      <Link
-        href={Routes.SectionDashboardPage({
-          projectSlug: projectSlug!,
-          sectionSlug: section.slug,
-        })}
-        classNameOverwrites="flex w-full h-full"
-      >
-        <div className="flex items-center justify-start gap-4">
-          <SectionMarker number={section.index} />
-          <h3 className="grow text-xl font-bold leading-tight">{section.title}</h3>
-        </div>
-      </Link>
-    </div>
+    <Link
+      href={Routes.SectionDashboardPage({
+        projectSlug: projectSlug!,
+        sectionSlug: section.slug,
+      })}
+      classNameOverwrites="flex items-center justify-start gap-4 rounded-md border border-gray-200 bg-white p-5 shadow-md hover:bg-gray-100 hover:text-rsv-blau"
+    >
+      <SectionMarker number={section.index} />
+      <h3 className="flex items-center pt-0.5 text-xl font-bold leading-tight">{section.title}</h3>
+    </Link>
   )
 }
