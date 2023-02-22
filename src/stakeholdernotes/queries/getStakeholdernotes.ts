cff4a068d3cf91b6d518935e0e4e4d56a78d5c5d
Sven König <sven@fixmycity.de>
2023-02-22 15:39:03 +0100

    auth: ...for stakeholders

--- src/stakeholdernotes/queries/getStakeholdernotes.ts
+++ src/stakeholdernotes/queries/getStakeholdernotes.ts
@@ -1,18 +1,26 @@
 import { resolver } from "@blitzjs/rpc"
 import { paginate } from "blitz"
 import db, { Prisma } from "db"
-import { z } from "zod"
+
+import { authorizeProjectAdmin } from "src/authorization"
 
 type GetStakeholdernotesInput = { sectionSlug: string } & Pick<
   Prisma.StakeholdernoteFindManyArgs,
   "where" | "orderBy" | "skip" | "take"
 >
 
-export default resolver.pipe(
-  resolver.authorize(),
-  async ({ sectionSlug, where, orderBy, skip = 0, take = 100 }: GetStakeholdernotesInput) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+const getProjectId = async (input: Record<string, any>) =>
+  (
+    await db.section.findFirstOrThrow({
+      where: { slug: input.sectionSlug },
+      select: { projectId: true },
+    })
+  ).projectId
 
+export default resolver.pipe(
+  // @ts-ignore
+  authorizeProjectAdmin(getProjectId),
+  async ({ sectionSlug, where, orderBy, skip = 0, take = 100 }: GetStakeholdernotesInput) => {
     const saveWhere = { section: { slug: sectionSlug }, ...where }
     const {
       items: stakeholdernotes,
