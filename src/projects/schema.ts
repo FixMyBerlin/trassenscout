34e8468ddcd259b3c7db0f9951ee24830ad4109b
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-02-24 10:16:31 +0100

    fix validation

--- src/projects/schema.ts
+++ src/projects/schema.ts
@@ -4,7 +4,7 @@ import { z } from "zod"
 export const ProjectSchema = z.object({
   slug: SlugSchema,
   title: NameSchema,
-  shortTitle: z.string().min(5, { message: "Pflichtfeld. Mindestens 5 Zeichen." }),
+  shortTitle: z.string().min(2, { message: "Pflichtfeld. Mindestens 2 Zeichen." }),
   description: z.string().nullish(),
   managerId: z.coerce.number(),
 })
