01f9519936ad1d7e12ca46cc9d622c68cca64f0c
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-02-27 18:28:25 +0100

    fix logo width

--- src/core/layouts/Navigation/NavigationGeneral/NavigationGeneralLogo.tsx
+++ src/core/layouts/Navigation/NavigationGeneral/NavigationGeneralLogo.tsx
@@ -3,13 +3,13 @@ import svgLogoTrassenscout from "../assets/trassenscout-logo-weiss.svg"
 
 export const NavigationGeneralLogo: React.FC = () => {
   return (
-    <div className="flex items-center">
+    <div className="flex items-end">
       <Image
         src={svgLogoTrassenscout}
         className="block h-8 w-auto text-rsv-ochre lg:hidden"
         alt="Trassenscout"
         height={32}
-        width={217}
+        width={120}
       />
       <span className="ml-2 text-xs uppercase text-gray-400">Alpha</span>
     </div>
