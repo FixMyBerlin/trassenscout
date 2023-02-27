8f3b388a1467841fbde5f4a6845e2763d0d406ae
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-02-27 18:28:08 +0100

    update icon

--- src/home/components/PageHomePublicContact.tsx
+++ src/home/components/PageHomePublicContact.tsx
@@ -1,3 +1,4 @@
+import { EnvelopeIcon } from "@heroicons/react/24/outline"
 import React from "react"
 import { LinkMail, LinkTel } from "src/core/components/links"
 
@@ -25,10 +26,10 @@ export const PageHomePublicContact: React.FC = () => {
   return (
     <section className="rounded-2xl border border-gray-100 p-6">
       <h2 className="flex items-center text-sm font-semibold text-gray-900">
-        <MailIcon />
+        <EnvelopeIcon className="h-6 w-6 flex-none text-gray-400" />
         <span className="mt-0.5 ml-3">Kontakt</span>
       </h2>
-      <p className="mt-2 text-sm leading-6 text-gray-600">
+      <p className="mt-2 text-sm leading-6">
         <strong>FixMyCity GmbH</strong>
         <br />
         <LinkMail>hello@trassenscout.de</LinkMail>
