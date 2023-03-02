ea992089ea1968dc35ac957f2e02a442a0df392d
Sven König <sven@fixmycity.de>
2023-03-02 21:20:07 +0100

    add .../files/upload

--- src/pages/[projectSlug]/files/new.tsx
+++ src/pages/[projectSlug]/files/new.tsx
@@ -1,7 +1,7 @@
 import { BlitzPage, Routes, useParam } from "@blitzjs/next"
 import { useMutation, useQuery } from "@blitzjs/rpc"
 import { useRouter } from "next/router"
-import { ChangeEventHandler, Suspense, useState } from "react"
+import { Suspense } from "react"
 import { Link } from "src/core/components/links"
 import { PageHeader } from "src/core/components/PageHeader"
 import { Spinner } from "src/core/components/Spinner"
@@ -10,11 +10,6 @@ import { FileForm, FORM_ERROR } from "src/files/components/FileForm"
 import createFile from "src/files/mutations/createFile"
 import { FileSchema } from "src/files/schema"
 import getProject from "src/projects/queries/getProject"
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
-import { s3Client, BUCKET } from "src/core/libs/s3Client"
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
-import { useS3Upload } from "next-s3-upload"
-import { Notice } from "src/core/components/Notice"
 
 const NewFileWithQuery = () => {
   const router = useRouter()
@@ -40,30 +35,6 @@ const NewFileWithQuery = () => {
     }
   }
 
-  let [imageUrl, setImageUrl] = useState()
-  let { uploadToS3 } = useS3Upload()
-
-  const [errorUserFeedback, setErrorUserFeedback] = useState("")
-  const [successUserFeedback, setSuccessUserFeedback] = useState("")
-
-  let handleFileChange = async (event: Event) => {
-    event?.target?.files && setImageUrl(event.target.files[0])
-  }
-  const uploadFile = async () => {
-    if (imageUrl) {
-      try {
-        let { url } = await uploadToS3(imageUrl)
-        setSuccessUserFeedback("Upload erfolgreich.")
-      } catch (e: Event) {
-        switch (e.message.split(":")[0]) {
-          case "Access Denied":
-            setErrorUserFeedback("Upload nicht erlaubt.")
-            break
-        }
-      }
-    }
-  }
-
   return (
     <>
       <MetaTags noindex title="Neues Dokument" />
@@ -75,68 +46,6 @@ const NewFileWithQuery = () => {
         // initialValues={{}} // Use only when custom initial values are needed
         onSubmit={handleSubmit}
       />
-
-      <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-        <label
-          htmlFor="cover-photo"
-          className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-        >
-          Datei ablegen
-        </label>
-        <div className="mt-1 sm:col-span-2 sm:mt-0">
-          <div className="flex max-w-lg justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-            <div className="space-y-1 text-center">
-              <svg
-                className="mx-auto h-12 w-12 text-gray-400"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 48 48"
-                aria-hidden="true"
-              >
-                <path
-                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <div className="flex text-sm text-gray-600">
-                <label
-                  htmlFor="file-upload"
-                  className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
-                >
-                  <span>Datei hochladen</span>
-                  <input
-                    id="file-upload"
-                    name="file-upload"
-                    type="file"
-                    className="sr-only"
-                    onChange={handleFileChange}
-                  />
-                </label>
-                <p className="pl-1">oder drag und drop</p>
-              </div>
-              <p className="text-xs text-gray-500">PNG, JPG, GIF up to ??? MB</p>
-            </div>
-          </div>
-        </div>
-        <button
-          className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          onClick={() => uploadFile()}
-        >
-          Hochladen
-        </button>
-        {successUserFeedback && (
-          <Notice type={"success"} title={"Erfolgeich"}>
-            {successUserFeedback}
-          </Notice>
-        )}
-        {errorUserFeedback && (
-          <Notice type={"error"} title={"Fehler"}>
-            {errorUserFeedback}
-          </Notice>
-        )}
-      </div>
     </>
   )
 }
---
+++ src/pages/[projectSlug]/files/upload.tsx
@@ -0,0 +1,150 @@
+import { BlitzPage, Routes, useParam } from "@blitzjs/next"
+import { useMutation, useQuery } from "@blitzjs/rpc"
+import { useRouter } from "next/router"
+import { Suspense, useState } from "react"
+import { Link } from "src/core/components/links"
+import { PageHeader } from "src/core/components/PageHeader"
+import { Spinner } from "src/core/components/Spinner"
+import { LayoutRs, MetaTags } from "src/core/layouts"
+import createFile from "src/files/mutations/createFile"
+import getProject from "src/projects/queries/getProject"
+import { useS3Upload } from "next-s3-upload"
+
+const UploadNewFileWithQuery = () => {
+  const router = useRouter()
+  const [createFileMutation] = useMutation(createFile)
+  const projectSlug = useParam("projectSlug", "string")
+  const sectionSlug = useParam("sectionSlug", "string")
+  const [project] = useQuery(getProject, { slug: projectSlug! })
+
+  const [fileToUpload, setFileToUpload] = useState<File | null>(null)
+  type FileUploadState =
+    | "INITIAL"
+    | "FILE_SELECTED"
+    | "FILE_UPLOADING"
+    | "FILE_UPLOADED"
+    | "FILE_SAVED"
+  const [uploadState, setUploadState] = useState<FileUploadState>("INITIAL")
+  const [fileUrl, setFileUrl] = useState<string | null>(null)
+  const { uploadToS3 } = useS3Upload()
+
+  let handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files as FileList
+    if (files[0]) {
+      setFileToUpload(files[0])
+      setUploadState("FILE_SELECTED")
+    } else {
+      setFileToUpload(null)
+      setUploadState("INITIAL")
+    }
+  }
+
+  const wait = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay))
+
+  const uploadFile = async () => {
+    setUploadState("FILE_UPLOADING")
+    let { url } = await uploadToS3(fileToUpload!)
+    setUploadState("FILE_UPLOADED")
+    setFileUrl(url)
+    const file = await createFileMutation({
+      title: fileToUpload!.name,
+      externalUrl: url,
+      projectId: project.id,
+    })
+    setUploadState("FILE_SAVED")
+    await wait(1000)
+    await router.push(
+      Routes.ShowFilePage({
+        projectSlug: projectSlug!,
+        sectionSlug: sectionSlug!,
+        fileId: file.id,
+      })
+    )
+  }
+
+  // @ts-ignore
+  const FileSelector = ({ onChange }) => (
+    <div className="mt-2 flex text-xl underline">
+      <label htmlFor="file-upload">
+        <span>Datei auswählen</span>
+        <input
+          id="file-upload"
+          name="file-upload"
+          type="file"
+          className="sr-only"
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  )
+
+  // @ts-ignore
+  const UploadButton = ({ onClick }) => (
+    <button className="mt-2 flex text-xl underline" onClick={onClick}>
+      Hochladen
+    </button>
+  )
+
+  return (
+    <>
+      <MetaTags noindex title="Neue Datei hochladen" />
+
+      <div>
+        <div className="border-2 border-blue-200 p-2">
+          <code>
+            <pre>state: {uploadState}</pre>
+            <br />
+            {["FILE_SELECTED", "FILE_UPLOADING", "FILE_UPLOADED", "FILE_SAVED"].includes(
+              uploadState
+            ) && (
+              <pre>
+                name: {fileToUpload!.name}
+                <br />
+                type: {fileToUpload!.type}
+                <br />
+                size: {fileToUpload!.size}
+                <br />
+                {["FILE_UPLOADED", "FILE_SAVED"].includes(uploadState) && (
+                  <>
+                    url: {fileUrl || "null"}
+                    <br />
+                  </>
+                )}
+              </pre>
+            )}
+          </code>
+        </div>
+
+        {["INITIAL", "FILE_SELECTED"].includes(uploadState) && (
+          <FileSelector onChange={handleFileChange} />
+        )}
+
+        {uploadState === "FILE_SELECTED" && (
+          <>
+            <UploadButton onClick={() => uploadFile()} />
+          </>
+        )}
+      </div>
+    </>
+  )
+}
+
+const UploadFilePage: BlitzPage = () => {
+  const projectSlug = useParam("projectSlug", "string")
+
+  return (
+    <LayoutRs>
+      <PageHeader title="Neue Datei hochladen" />
+      <Suspense fallback={<Spinner page />}>
+        <UploadNewFileWithQuery />
+      </Suspense>
+      <p className="mt-5">
+        <Link href={Routes.FilesPage({ projectSlug: projectSlug! })}>Zurück zu Dokumenten</Link>
+      </p>
+    </LayoutRs>
+  )
+}
+
+UploadFilePage.authenticate = true
+
+export default UploadFilePage
