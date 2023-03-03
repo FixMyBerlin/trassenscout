c78427568535abd6f4d0623480c906159652dc7f
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-03 15:56:26 +0100

    fix title, remove fallback team page

--- src/pages/[projectSlug]/contacts/team.tsx
+++ src/pages/[projectSlug]/contacts/team.tsx
@@ -26,22 +26,6 @@ export const ProjectTeamWithQuery = () => {
   const { Membership } = project
   const teamContacts = Membership.map((item: { user: User }) => item.user)
 
-  // TODO at the moment this fallback shows at least the manager of the project, in case no memberships exist
-  // - in the future it should not be possible to have a project without memberships - at least manager should have one
-  if (!teamContacts.length) {
-    return (
-      <div className="mt-8 flex flex-col">
-        <PageHeader
-          title="Das Projektteam"
-          description={`Dieser Bereich hilft Ihnen dabei wichtige Informationen und Kontakte der Beteiligten des Projektes ${project.title} zu finden.`}
-        />
-        <p className="text-center text-xl text-gray-500">
-          <ContactListTeam contacts={[user!]} />
-        </p>
-      </div>
-    )
-  }
-
   return (
     <div className="mt-8 flex flex-col">
       <PageHeader
@@ -49,7 +33,6 @@ export const ProjectTeamWithQuery = () => {
         description={`Dieser Bereich hilft Ihnen dabei wichtige Informationen und Kontakte der Beteiligten des Projektes ${project.title} zu finden.`}
       />
       <ContactListTeam contacts={teamContacts} />
-      {/* TODO query memberships for project and replace contacts with users with membership */}
       <div className="mt-6"></div>
     </div>
   )
@@ -59,7 +42,7 @@ const ProjectTeamPage: BlitzPage = () => {
   const projectSlug = useParam("projectSlug", "string")
   return (
     <LayoutRs>
-      <MetaTags noindex title="Kontakte" />
+      <MetaTags noindex title="Projektteam" />
 
       <Suspense fallback={<Spinner page />}>
         <ProjectTeamWithQuery />
