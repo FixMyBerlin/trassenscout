c2c6a6c804c55b28e8761a3d33bc57c6d3830f5a
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-15 13:09:25 +0100

    style, move participation form components

---
+++ src/participation/components/form/ParticipationLabeledCheckbox.tsx
@@ -0,0 +1,65 @@
+import { ErrorMessage } from "@hookform/error-message"
+import clsx from "clsx"
+import { ComponentPropsWithoutRef, forwardRef, PropsWithoutRef } from "react"
+import { useFormContext } from "react-hook-form"
+
+export interface TParticipationLabeledCheckbox
+  extends PropsWithoutRef<JSX.IntrinsicElements["input"]> {
+  /** Field name. */
+  name: string
+  /** Field label. */
+  label: string
+  /** Help text below field label. */
+  help?: string
+  outerProps?: PropsWithoutRef<JSX.IntrinsicElements["div"]>
+  labelProps?: ComponentPropsWithoutRef<"label">
+}
+
+export const ParticipationLabeledCheckbox = forwardRef<
+  HTMLInputElement,
+  TParticipationLabeledCheckbox
+>(({ name, label, help, outerProps, labelProps, ...props }, ref) => {
+  const {
+    register,
+    formState: { isSubmitting, errors },
+  } = useFormContext()
+
+  const hasError = Boolean(errors[name])
+
+  return (
+    <div {...outerProps} className={clsx(outerProps?.className, "flex items-start")}>
+      <div className="flex h-5 items-center">
+        <input
+          type="checkbox"
+          disabled={isSubmitting}
+          {...register(name)}
+          id={name}
+          {...props}
+          className={clsx(
+            "h-4 w-4 rounded",
+            hasError
+              ? "border-red-800 text-red-500 shadow-sm shadow-red-200 focus:ring-red-800"
+              : "border-gray-300 text-pink-600 focus:ring-pink-500"
+          )}
+        />
+      </div>
+      <label
+        {...labelProps}
+        htmlFor={name}
+        className="ml-3 block cursor-pointer text-sm font-medium text-gray-700 hover:text-gray-800"
+      >
+        {label}
+        {help && <div className="m-0 text-gray-500">{help}</div>}
+        <ErrorMessage
+          render={({ message }) => (
+            <p role="alert" className="m-0 text-sm text-red-800">
+              {message}
+            </p>
+          )}
+          errors={errors}
+          name={name}
+        />
+      </label>
+    </div>
+  )
+})
---
+++ src/participation/components/form/ParticipationLabeledCheckboxGroup.tsx
@@ -0,0 +1,18 @@
+import clsx from "clsx"
+import React from "react"
+import { ParticipationLabeledCheckbox, TParticipationLabeledCheckbox } from "./ParticipationLabeledCheckbox"
+
+type Props = {
+  items: TParticipationLabeledCheckbox[]
+  className?: string
+}
+
+export const ParticipationLabeledCheckboxGroup: React.FC<Props> = ({ items, className }) => {
+  return (
+    <div className={clsx(className, "space-y-3")}>
+      {items.map((item) => {
+        return <ParticipationLabeledCheckbox key={item.name} {...item} />
+      })}
+    </div>
+  )
+}
