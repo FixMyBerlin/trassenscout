80a11a8c09c7bcd2927457abc6d475535682a2cd
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-16 16:34:17 +0100

    create ParticipationButton

---
+++ src/participation/components/core/ParticipationButton.tsx
@@ -0,0 +1,40 @@
+import clsx from "clsx"
+import { pinkButtonStyles, whiteButtonStyles } from "src/core/components/links"
+import { TButton } from "../pages/Page"
+export { FORM_ERROR } from "src/core/components/forms"
+
+export type TParticipationButton = {
+  button: TButton
+  buttonActions: { next: any; back: any; reset: any }
+}
+
+export const ParticipationButton: React.FC<TParticipationButton> = ({ button, buttonActions }) => {
+  const { label, color, onClick } = button
+  const buttonStyles = clsx("px-12", color === "white" ? whiteButtonStyles : pinkButtonStyles)
+
+  if (onClick.action === "submit")
+    return (
+      <button type="submit" className={buttonStyles}>
+        {label.de}
+      </button>
+    )
+
+  let buttonActionSelect: any
+  switch (onClick.action) {
+    case "reset":
+      buttonActionSelect = buttonActions.reset
+      break
+    case "nextPage":
+      buttonActionSelect = buttonActions.next
+      break
+    case "previousPage":
+      buttonActionSelect = buttonActions.back
+      break
+  }
+
+  return (
+    <button type="button" className={buttonStyles} onClick={buttonActionSelect}>
+      {label.de}
+    </button>
+  )
+}
