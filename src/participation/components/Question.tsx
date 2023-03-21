b26fc8fe10ed4025bf9898e8ea5092d46861cb51
Sven König <sven@fixmycity.de>
2023-03-21 14:01:21 +0100

    create one component for each "component": in survey definition

--- src/participation/components/Question.tsx
+++ src/participation/components/Question.tsx
@@ -5,51 +5,49 @@ import { ParticipationLabeledTextareaField } from "./form/ParticipationLabeledTe
 import { TQuestion } from "./pages/Page"
 export { FORM_ERROR } from "src/core/components/forms"
 
-type QuestionGroupProps = { question: TQuestion }
+const SingleResponseComponent = ({ id, responses }) => (
+  <ParticipationLabeledRadiobuttonGroup
+    items={responses.map((item) => ({
+      scope: String(id),
+      name: item.text.de,
+      label: item.text.de,
+      value: item.text.de,
+    }))}
+  />
+)
 
-const QuestionGroup: React.FC<QuestionGroupProps> = ({ question }) => {
-  const { id, component, props } = question
-  const { responses } = props
-  if (!responses) return null
-  if (component === "singleResponse")
-    return (
-      <ParticipationLabeledRadiobuttonGroup
-        key={id}
-        items={responses.map((item) => ({
-          scope: String(id),
-          name: item.text.de,
-          label: item.text.de,
-          value: item.text.de,
-        }))}
-      />
-    )
-  if (component === "multipleResponse")
-    return (
-      <ParticipationLabeledCheckboxGroup
-        key={id}
-        items={responses.map((item) => ({
-          name: item.text.de,
-          label: item.text.de,
-        }))}
-      />
-    )
-    return (
-      <>
-        <ParticipationLabeledTextareaField name={String(id)} label={""} />
-        <p className="mt-2 text-right text-sm text-gray-500">Max. 2000 Zeichen</p>
-      </>
-    )
+const MultipleResponseComponent = ({ id, responses }) => (
+  <ParticipationLabeledCheckboxGroup
+    key={id}
+    items={responses.map((item) => ({
+      name: item.text.de,
+      label: item.text.de,
+    }))}
+  />
+)
+
+const TextResponseComponent = ({ id }) => (
+  <>
+    <ParticipationLabeledTextareaField name={String(id)} label={""} />
+    <p className="mt-2 text-right text-sm text-gray-500">Max. 2000 Zeichen</p>
+  </>
+)
+
+const components = {
+  singleResponse: SingleResponseComponent,
+  multipleResponse: MultipleResponseComponent,
+  text: TextResponseComponent,
 }
 
 type Props = { question: TQuestion }
 
 export const Question: React.FC<Props> = ({ question }) => {
-  if (!question) return null
-  const { id, label } = question
+  const { id, label, component, props } = question
+  const Component = components[component]
   return (
-    <div key={question.id}>
-      <H2>{question.label.de}</H2>
-      <QuestionGroup question={question} />
+    <div key={id}>
+      <H2>{label.de}</H2>
+      <Component id={id} {...props} />
     </div>
   )
 }
