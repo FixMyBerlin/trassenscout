60564b1cc315012ef7f58b26dd3ba5ac84d3a275
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-21 17:13:20 +0100

    update Layout for desktop

--- src/participation/components/core/LayoutParticipation.tsx
+++ src/participation/components/core/LayoutParticipation.tsx
@@ -4,30 +4,20 @@ import { TailwindResponsiveHelper } from "src/core/layouts/TailwindResponsiveHel
 import { FooterParticipation } from "./FooterParticipation"
 
 type Props = {
-  fullWidth?: boolean
   children?: React.ReactNode
   navigation?: React.ReactNode
 }
 
-export const LayoutParticipation: BlitzLayout<Props> = ({
-  fullWidth = false,
-  children,
-  navigation,
-}) => {
+export const LayoutParticipation: BlitzLayout<Props> = ({ children, navigation }) => {
   return (
     <>
       <Head>
-        {/* Reminder: We cannot use this to set the <body class>. See index.css for our workaround. */}
-        {/* /src/core/layouts/Navigation/assets/trassenscout-logo-bildmarke.svg */}
+        {/* TODO */}
         <link rel="icon" href="favicon.svg" type="image/svg+xml" />
       </Head>
       {navigation}
       <div className="relative flex h-full flex-col overflow-x-hidden">
-        {fullWidth ? (
-          <main className="w-full">{children}</main>
-        ) : (
-          <main className="mx-auto w-full max-w-7xl px-6 pb-16 md:px-8">{children}</main>
-        )}
+        <main className="mx-auto w-full max-w-3xl px-3 pb-40">{children}</main>
       </div>
 
       <FooterParticipation />
