6604ba174ab550c3b995cffa10b4d98b05472aa2
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-27 15:05:25 +0200

    update More

--- src/participation/components/More.tsx
+++ src/participation/components/More.tsx
@@ -1,20 +1,17 @@
 import { useContext, useEffect, useState } from "react"
-import { useForm, useFormContext } from "react-hook-form"
 import { Form } from "src/core/components/forms"
-import { pinkButtonStyles, whiteButtonStyles } from "src/core/components/links"
 import { ProgressContext } from "src/pages/beteiligung"
 import { ParticipationButton } from "./core/ParticipationButton"
 import { ScreenHeaderParticipation } from "./core/ScreenHeaderParticipation"
+import { ParticipationH2 } from "./core/Text"
 import { ParticipationLabeledRadiobuttonGroup } from "./form/ParticipationLabeledRadiobuttonGroup"
-import { Question } from "./survey/Question"
 
 export { FORM_ERROR } from "src/core/components/forms"
 
 type Props = {
   onClickMore: any
   onClickFinish: any
-
-  more: any
+  more: any // TODO
 }
 
 export const More: React.FC<Props> = ({ more, onClickMore, onClickFinish }) => {
@@ -30,30 +27,27 @@ export const More: React.FC<Props> = ({ more, onClickMore, onClickFinish }) => {
   const button = buttons[0]
 
   const handleChange = (values) => {
-    setIsFeedback(values.feedback === question.props.responses[0].text.de)
+    setIsFeedback(Number(values.feedback) === question.props.responses[0].id)
     setIsDirty(
-      values.feedback === question.props.responses[0].text.de ||
-        values.feedback === question.props.responses[1].text.de
+      Number(values.feedback) === question.props.responses[0].id ||
+        Number(values.feedback) === question.props.responses[1].id
     )
   }
 
   return (
     <Form onSubmit={onClickFinish} onChangeValues={handleChange}>
       <ScreenHeaderParticipation title={title.de} description={description.de} />
+      <ParticipationH2>{question.label.de}</ParticipationH2>
       <ParticipationLabeledRadiobuttonGroup
         items={question.props.responses.map((item) => ({
           scope: "feedback",
-          name: item.text.de,
+          name: String(item.id),
           label: item.text.de,
-          value: item.text.de,
+          value: item.id,
         }))}
       />
 
-      <ParticipationButton
-        disabled={!isDirty}
-        className={pinkButtonStyles}
-        onClick={isFeedback ? onClickMore : onClickFinish}
-      >
+      <ParticipationButton disabled={!isDirty} onClick={isFeedback ? onClickMore : onClickFinish}>
         {button.label.de}
       </ParticipationButton>
     </Form>
