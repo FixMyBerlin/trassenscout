382bf4cc10d8d961890b280edaef335a1995ac70
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-28 16:38:48 +0200

    update type and default value ProgressContext

--- src/participation/context/contexts.ts
+++ src/participation/context/contexts.ts
@@ -1,4 +1,13 @@
 import { createContext } from "react"
 
-export const ProgressContext = createContext({ current: 0, total: 0 })
+export type TProgressContext = {
+  progress: { current: number; total: number }
+  setProgress: ({ current, total }: { current: number; total: number }) => void
+}
+
+export const ProgressContext = createContext<TProgressContext>({
+  progress: { current: 0, total: 0 },
+  setProgress: () => {},
+})
+
 export const PinContext = createContext(null)
