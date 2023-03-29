16be550dd8eb06147a739a4f56151a24c36455d5
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-29 15:18:48 +0200

    update Done as placeholder

--- src/participation/components/Done.tsx
+++ src/participation/components/Done.tsx
@@ -1,3 +1,23 @@
 export { FORM_ERROR } from "src/core/components/forms"
+import clsx from "clsx"
+import { Link, whiteButtonStyles } from "src/core/components/links"
+import { ParticipationButtonWrapper } from "./core/ParticipationButtonWrapper"
+import { ScreenHeaderParticipation } from "./core/ScreenHeaderParticipation"
 
-export const Done = () => <h1>Done</h1>
+export const Done = () => {
+  return (
+    <section>
+      <ScreenHeaderParticipation
+        title="Vielen Dank!"
+        description="Ihre Emailadresse wurde vermerkt."
+      />
+
+      <Link
+        className={clsx(whiteButtonStyles, "mt-32")}
+        href="https://radschnellweg8-lb-wn.de/beteiligung"
+      >
+        Zurück Startseite
+      </Link>
+    </section>
+  )
+}
