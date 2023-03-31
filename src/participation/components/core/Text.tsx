aa2879faf3a37e769c789b7436cec1d0aa9f0c22
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-03-31 11:44:30 +0200

    update H1

--- src/participation/components/core/Text.tsx
+++ src/participation/components/core/Text.tsx
@@ -8,7 +8,12 @@ type Props = {
 
 export const ParticipationH1: React.FC<Props> = ({ className, children }) => {
   return (
-    <h1 className={clsx("mb-8 pt-10 text-3xl font-extrabold text-gray-900 sm:text-5xl", className)}>
+    <h1
+      className={clsx(
+        "mb-8 pt-10 text-3xl font-extrabold text-gray-900 sm:text-5xl md:text-4xl",
+        className
+      )}
+    >
       {children}
     </h1>
   )
