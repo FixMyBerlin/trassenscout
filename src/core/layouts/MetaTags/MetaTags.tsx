f7ab070774ad2bd7d56c16c08750122cdc425b07
Sven König <sven@fixmycity.de>
2023-04-03 14:55:59 +0200

    set noindex to false is not production

--- src/core/layouts/MetaTags/MetaTags.tsx
+++ src/core/layouts/MetaTags/MetaTags.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import React from "react"
 import SocialSharingImage from "./assets/default.png"
+import { isProduction } from "../../utils"
 
 const seoDefaultValues = {
   defaultTitle: "Trassenscout",
@@ -19,8 +20,7 @@ type Props = {
 export const MetaTags: React.FC<Props> = ({ noindex, title, description, image, article }) => {
   const { defaultTitle, defaultDescription, baseUrl } = seoDefaultValues
 
-  // On Production, take the prop or `false`. Staging (and everythign else) is set to `true`
-  noindex = true // isProd ? noindex ?? false : true // TODO: `isProd` in src/core/utils/isEnv.ts is not implemented, yet
+  if (noindex === undefined) noindex = !isProduction
 
   const seo = {
     title: title || defaultTitle,
@@ -28,7 +28,7 @@ export const MetaTags: React.FC<Props> = ({ noindex, title, description, image, 
     image: `${baseUrl}${image || SocialSharingImage}`,
   }
 
-  if (noindex === true) {
+  if (noindex) {
     return (
       <Head>
         <title>{seo.title}</title>
