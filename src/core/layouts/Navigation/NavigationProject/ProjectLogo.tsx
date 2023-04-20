bb5f4be9bcef70705fff1418a92703999798cd9c
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-04-20 09:06:29 +0200

    update logo layout, remove unneeded code

--- src/core/layouts/Navigation/NavigationProject/ProjectLogo.tsx
+++ src/core/layouts/Navigation/NavigationProject/ProjectLogo.tsx
@@ -17,33 +17,24 @@ export const ProjectLogoWithQuery: React.FC<Props> = ({ className, defaultImg = 
   const projectSlug = useParam("projectSlug", "string")
   const [project] = useQuery(getProject, { slug: projectSlug })
 
-  const regionlogoWhiteBackgroundRequired = false
-  // TODO
-
   if (!project.logoSrc && !defaultImg) return null
 
   return (
     <>
       {project.logoSrc ? (
-        <div
-          className={clsx({
-            "rounded-sm bg-white/90 px-1 py-1": regionlogoWhiteBackgroundRequired,
-          })}
-        >
-          <div className={clsx(className, "relative h-12 w-12")}>
-            <Image
-              className="object-contain"
-              layout="fill"
-              src={getImageSrc(project.logoSrc)}
-              alt="Projektlogo"
-            />
-          </div>
+        <div className={clsx(className, "relative h-12 w-12")}>
+          <Image
+            className="object-contain"
+            layout="fill"
+            src={getImageSrc(project.logoSrc)}
+            alt="Projektlogo"
+          />
         </div>
       ) : (
         <div className={className}>
           <Image
             src={svgLogoTrassenscout}
-            className="block h-8 w-auto text-yellow-500 lg:hidden"
+            className="text-yellow-500"
             alt="Trassenscout"
             height={36}
             width={48}
