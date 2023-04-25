248acf24a3f125e06a82958616884565cc59df8a
Tobias <tobias@fixmycity.de>
2023-04-25 15:53:14 +0200

    Disable `isStaging`
    Ping https://github.com/FixMyBerlin/private-issues/issues/567

--- src/core/utils/isEnv.ts
+++ src/core/utils/isEnv.ts
@@ -1,9 +1,11 @@
 export const isProduction =
   process.env.NODE_ENV === "production" && process.env.APP_ORIGIN === "http://trassenscout.de"
 
-export const isStaging =
-  process.env.NODE_ENV === "production" &&
-  process.env.APP_ORIGIN === "http://staging.trassenscout.de"
+
+// This does not handle tinkering.trassenscout, yet
+// export const isStaging =
+//   process.env.NODE_ENV === "production" &&
+//   process.env.APP_ORIGIN === "http://staging.trassenscout.de"
 
 export const isDev = process.env.NODE_ENV === "development"
 
