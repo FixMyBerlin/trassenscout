1e4f2cdb4d39c322e3a12054ba031fe83fa1ad2e
Sven König <sven@fixmycity.de>
2023-05-03 00:06:28 +0200

    format isEnv

--- src/core/utils/isEnv.ts
+++ src/core/utils/isEnv.ts
@@ -1,7 +1,6 @@
 export const isProduction =
   process.env.NODE_ENV === "production" && process.env.APP_ORIGIN === "http://trassenscout.de"
 
-
 // This does not handle tinkering.trassenscout, yet
 // export const isStaging =
 //   process.env.NODE_ENV === "production" &&
