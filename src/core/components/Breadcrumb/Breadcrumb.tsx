cfcb1fddeb18c7792aae54af72cb0b27fbda4c91
Tobias <tobias@fixmycity.de>
2023-05-08 14:54:34 +0200

    Update Breadcrumb styles

--- src/core/components/Breadcrumb/Breadcrumb.tsx
+++ src/core/components/Breadcrumb/Breadcrumb.tsx
@@ -1,4 +1,4 @@
-import { HomeIcon } from "@heroicons/react/20/solid"
+import { ChevronRightIcon, HomeIcon } from "@heroicons/react/20/solid"
 import { Link } from "../links"
 import { RouteUrlObject } from "blitz"
 import { useSlugs } from "src/core/hooks"
@@ -10,26 +10,17 @@ import getSubsection from "src/subsections/queries/getSubsection"
 
 const BreadcrumbStep: React.FC<{ title: string; route?: RouteUrlObject }> = ({ title, route }) => {
   return (
-    <li>
-      <div className="flex items-center">
-        <svg
-          className="h-5 w-5 flex-shrink-0 text-gray-300"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          aria-hidden="true"
-        >
-          <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
-        </svg>
-        {typeof route === "object" ? (
-          <Link href={route} className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">
-            {title}
-          </Link>
-        ) : (
-          <span className="ml-4 text-sm font-medium text-gray-500" aria-current="page">
-            {title}
-          </span>
-        )}
-      </div>
+    <li className="flex items-center">
+      <ChevronRightIcon className="mb-0.5 h-5 w-5 flex-shrink-0 text-gray-900" />
+      {typeof route === "object" ? (
+        <Link href={route} className="px-4 text-sm font-medium text-gray-500 hover:text-gray-700">
+          {title}
+        </Link>
+      ) : (
+        <span className="select-none px-4 text-sm font-medium text-gray-500" aria-current="page">
+          {title}
+        </span>
+      )}
     </li>
   )
 }
@@ -60,14 +51,12 @@ export const Breadcrumb: React.FC = () => {
 
   return (
     <nav className="mt-10 flex" aria-label="Breadcrumb">
-      <ol role="list" className="flex items-center space-x-4">
-        <li>
-          <div>
-            <a href="/" className="text-gray-400 hover:text-gray-500">
-              <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
-              <span className="sr-only">Projektübersicht</span>
-            </a>
-          </div>
+      <ol className="flex items-center">
+        <li className="pr-4">
+          <Link href="/">
+            <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+            <span className="sr-only">Projektübersicht</span>
+          </Link>
         </li>
         {project && (
           <BreadcrumbStep
