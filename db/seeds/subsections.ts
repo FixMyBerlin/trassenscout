ee3a9bf22cccc6381a6bbc9a37fba14551ac559a
Sven König <sven@fixmycity.de>
2023-05-09 14:05:57 +0200

    convert geometry to json

--- db/seeds/subsections.ts
+++ db/seeds/subsections.ts
@@ -10,7 +10,7 @@ const seedSubsections = async () => {
       start: "Dovestraẞe",
       end: "Kanzlerpark",
       labelPos: "top",
-      geometry: JSON.stringify([
+      geometry: [
         [13.317392954811083, 52.52039883952099],
         [13.318029192341385, 52.52262482165125],
         [13.322641914435906, 52.5248506909908],
@@ -22,7 +22,7 @@ const seedSubsections = async () => {
         [13.350954484534668, 52.51914062581497],
         [13.357157800454104, 52.517204842057566],
         [13.363361116374904, 52.519430986022115],
-      ]),
+      ],
       managerId: 1,
       sectionId: 1,
       subsubsections: {
@@ -32,10 +32,10 @@ const seedSubsections = async () => {
             title: "Teilplanung 1",
             description: "Das Hansaviertel ist ein Stadtteil im Zentrum von Berlin - Teilplanung 1",
             labelPos: "topLeft",
-            geometry: JSON.stringify([
+            geometry: [
               [13.363361116374904, 52.519430986022115],
               [13.357157800454104, 52.517204842057566],
-            ]),
+            ],
             guidance: "* guidance 1 *",
             task: "* task 1 *",
             length: 0.487,
@@ -46,11 +46,11 @@ const seedSubsections = async () => {
             title: "Teilplanung 2",
             description: "Das Hansaviertel ist ein Stadtteil im Zentrum von Berlin - Teilplanung 2",
             labelPos: "bottomLeft",
-            geometry: JSON.stringify([
+            geometry: [
               [13.350954484534668, 52.51914062581497],
               [13.345069287379602, 52.52262482165125],
               [13.33966126837197, 52.52233448255228],
-            ]),
+            ],
             guidance: "* guidance 2 *",
             task: "* task 2 *",
             length: 0.922,
@@ -61,12 +61,12 @@ const seedSubsections = async () => {
             title: "Teilplanung 3",
             description: "Das Hansaviertel ist ein Stadtteil im Zentrum von Berlin - Teilplanung 3",
             labelPos: "left",
-            geometry: JSON.stringify([
+            geometry: [
               [13.334253249364252, 52.51701125899095],
               [13.329481467886865, 52.5184631112015],
               [13.327890874060671, 52.523108715884604],
               [13.322641914435906, 52.5248506909908],
-            ]),
+            ],
             guidance: "* guidance 3 *",
             task: "* task 3 *",
             length: 1.293,
@@ -82,7 +82,7 @@ const seedSubsections = async () => {
       start: "Kanzlerpark",
       end: "Ebertsbrücke",
       labelPos: "top",
-      geometry: JSON.stringify([
+      geometry: [
         [13.392787102151175, 52.52243126246529],
         [13.387538142526438, 52.52204414153442],
         [13.381493885988561, 52.51904383865252],
@@ -90,7 +90,7 @@ const seedSubsections = async () => {
         [13.375131510684866, 52.52233448255228],
         [13.369246313529857, 52.52252804216468],
         [13.36352017575777, 52.519430986022115],
-      ]),
+      ],
       managerId: 1,
       sectionId: 1,
     },
@@ -101,12 +101,12 @@ const seedSubsections = async () => {
       start: "Ebertsbrücke",
       end: "Fischerinsel",
       labelPos: "topRight",
-      geometry: JSON.stringify([
+      geometry: [
         [13.410442693616204, 52.514591398693426],
         [13.405352793374334, 52.51565615364305],
         [13.399308536836458, 52.521366671697535],
         [13.392787102151175, 52.52252804216468],
-      ]),
+      ],
       managerId: 1,
       sectionId: 1,
     },
@@ -117,12 +117,12 @@ const seedSubsections = async () => {
       start: "Fischerinsel",
       end: "Schleusenufer",
       labelPos: "topRight",
-      geometry: JSON.stringify([
+      geometry: [
         [13.41060175299907, 52.514591398693426],
         [13.415532593858103, 52.51488178896585],
         [13.421258731631525, 52.51333301867112],
         [13.451639073702552, 52.499875779590525],
-      ]),
+      ],
       managerId: 1,
       sectionId: 1,
     },
@@ -134,12 +134,12 @@ const seedSubsections = async () => {
       start: "Dovestraẞe",
       end: "Lützowplatz",
       labelPos: "topRight",
-      geometry: JSON.stringify([
+      geometry: [
         [13.317597867354067, 52.52004509035683],
         [13.3314161960285, 52.51250603081962],
         [13.33697529376991, 52.51115272935999],
         [13.35222310471977, 52.5057391067665],
-      ]),
+      ],
       managerId: 2,
       sectionId: 2,
     },
@@ -150,7 +150,7 @@ const seedSubsections = async () => {
       start: "Lützowplatz",
       end: "Mehringdamm",
       labelPos: "topRight",
-      geometry: JSON.stringify([
+      geometry: [
         [13.35206427335541, 52.505835784446134],
         [13.357305708370632, 52.50670587399276],
         [13.362705974747598, 52.5055457507697],
@@ -158,7 +158,7 @@ const seedSubsections = async () => {
         [13.37525365250832, 52.502645308750346],
         [13.379859762067383, 52.49916452570534],
         [13.388754318454033, 52.49800420344744],
-      ]),
+      ],
       managerId: 2,
       sectionId: 2,
     },
@@ -169,12 +169,12 @@ const seedSubsections = async () => {
       start: "Mehringdamm",
       end: "Kottbusser Damm",
       labelPos: "topRight",
-      geometry: JSON.stringify([
+      geometry: [
         [13.388480650254337, 52.497979825501545],
         [13.395628061636813, 52.49788313054404],
         [13.41278184895532, 52.4950788842759],
         [13.41977042897335, 52.49614258494796],
-      ]),
+      ],
       managerId: 2,
       sectionId: 2,
     },
@@ -185,11 +185,11 @@ const seedSubsections = async () => {
       start: "Kottbusser Damm",
       end: "Schleusenufer",
       labelPos: "bottom",
-      geometry: JSON.stringify([
+      geometry: [
         [13.41977042897335, 52.49633598230557],
         [13.439306686753582, 52.4902435569129],
         [13.451695533149916, 52.49991367999152],
-      ]),
+      ],
       managerId: 2,
       sectionId: 2,
     },
@@ -201,11 +201,11 @@ const seedSubsections = async () => {
       start: "start abschnitt",
       end: "end abschnitt",
       labelPos: "top",
-      geometry: JSON.stringify([
+      geometry: [
         [13.41977042897335, 52.49633598230557],
         [13.439306686753582, 52.4902435569129],
         [13.451695533149916, 52.49991367999152],
-      ]),
+      ],
       managerId: 2,
       sectionId: 3,
     },
