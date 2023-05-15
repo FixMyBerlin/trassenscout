305eee3af98d2825f30a6a58ed8953a136bcf70b
Tobias <tobias@fixmycity.de>
2023-05-15 12:58:05 +0200

    Update packages minor (except blitz)
    `npx npm-check-updates -u --target minor`
    
    - Remove tailwind package that is not needed anymore
    - Do not update blitz js because that needs more testing…

--- tailwind.config.js
+++ tailwind.config.js
@@ -91,9 +91,5 @@ module.exports = {
       mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/forms"),
-  ],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 }
