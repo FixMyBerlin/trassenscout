b47d8777237391aaed03ff73a002eb4396f24fcb
Tobias <tobias@fixmycity.de>
2023-05-17 13:46:49 +0200

    TipMarker: Move styles to TW to remove ts-ignore

--- src/core/components/Map/TipMarker.tsx
+++ src/core/components/Map/TipMarker.tsx
@@ -34,29 +34,29 @@ const tipElements = {
   right: createSvg({ top: 5, left: 0 }, 270, pathSide),
 }
 
-const divStyle = {
-  position: "absolute",
-  border: "1px solid #999",
-  backgroundColor: "white",
-  borderRadius: 6,
+const shadow = {
   boxShadow: "2px 2px 4px 0px rgba(0, 0, 0, 0.25)",
 }
 const divStyles = {
-  bottomRight: { ...divStyle, top: 10, left: 10 },
-  bottom: { ...divStyle, top: 14, left: 0, transform: "translateX(-50%)" },
-  bottomLeft: { ...divStyle, top: 10, right: 10 },
-  left: { ...divStyle, top: 0, right: 14, transform: "translateY(-50%)" },
-  topLeft: { ...divStyle, bottom: 10, right: 10 },
-  top: { ...divStyle, bottom: 14, left: 0, transform: "translateX(-50%)" },
-  topRight: { ...divStyle, bottom: 10, left: 10 },
-  right: { ...divStyle, top: 0, left: 14, transform: "translateY(-50%)" },
+  bottomRight: { ...shadow, top: 10, left: 10 },
+  bottom: { ...shadow, top: 14, left: 0, transform: "translateX(-50%)" },
+  bottomLeft: { ...shadow, top: 10, right: 10 },
+  left: { ...shadow, top: 0, right: 14, transform: "translateY(-50%)" },
+  topLeft: { ...shadow, bottom: 10, right: 10 },
+  top: { ...shadow, bottom: 14, left: 0, transform: "translateX(-50%)" },
+  topRight: { ...shadow, bottom: 10, left: 10 },
+  right: { ...shadow, top: 0, left: 14, transform: "translateY(-50%)" },
 }
 
 export const TipMarker: React.FC<Props> = ({ anchor, children, ...props }) => {
   return (
     <div className="cursor-pointer whitespace-nowrap" {...props}>
-      {/* @ts-ignore TODO: fix this */}
-      <div style={divStyles[anchor]}>{children}</div>
+      <div
+        style={divStyles[anchor]}
+        className="absolute rounded-md border border-gray-400 bg-white"
+      >
+        {children}
+      </div>
       {tipElements[anchor]}
     </div>
   )
