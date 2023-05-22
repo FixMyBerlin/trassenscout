fa7df7ebb1f582df5640087081fd699d5239231b
Tobias <tobias@fixmycity.de>
2023-05-22 22:01:42 +0200

    Cleanup unused JsonValue

--- src/core/utils/schema-shared.ts
+++ src/core/utils/schema-shared.ts
@@ -1,4 +1,3 @@
-import { Prisma } from "db"
 import { z } from "zod"
 
 export const SlugSchema = z
@@ -7,14 +6,3 @@ export const SlugSchema = z
   .regex(/^[a-z0-9_-]*$/, { message: "Pflichtfeld. Erlaubte Zeichen a-z, 0-9, -, _." })
 
 export const NameSchema = z.string().min(5, { message: "Pflichtfeld. Mindestens 5 Zeichen." })
-
-// Thanks to https://github.com/chrishoermann/zod-prisma-types#field-validators
-export const JsonValue: z.ZodType<Prisma.JsonValue> = z
-  .union([
-    z.string(),
-    z.number(),
-    z.boolean(),
-    z.lazy(() => z.array(JsonValue)),
-    z.lazy(() => z.record(JsonValue)),
-  ])
-  .nullable()
