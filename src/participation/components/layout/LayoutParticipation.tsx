2d96ccd185657f1f45d58f653d1abc1ce28fbb40
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-06-07 15:02:40 +0200

    add canonicalUrl in surveyDefinition

--- src/participation/components/layout/LayoutParticipation.tsx
+++ src/participation/components/layout/LayoutParticipation.tsx
@@ -9,9 +9,10 @@ import { HeaderParticipation } from "./HeaderParticipation"
 type Props = {
   faviconUrl: string
   children?: React.ReactNode
+  canonicalUrl?: string
 }
 
-export const LayoutParticipation: BlitzLayout<Props> = ({ faviconUrl, children }) => {
+export const LayoutParticipation: BlitzLayout<Props> = ({ faviconUrl, children, canonicalUrl }) => {
   const extension = new URL(faviconUrl).pathname.split(".").at(-1)
   const mimetype =
     { ico: "image/x-icon", svg: "image/svg+xml", jpg: "image/jpeg" }[extension!] ||
@@ -20,7 +21,7 @@ export const LayoutParticipation: BlitzLayout<Props> = ({ faviconUrl, children }
     <>
       <Head>
         <link rel="icon" href={faviconUrl} type={mimetype} />
-        <link rel="canonical" href="https://radschnellweg8-lb-wn.de/beteiligung/" />
+        {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
       </Head>
       <div className="relative flex h-full flex-col overflow-x-hidden">
         <MetaTags noindex title="Beteiligung RS8" />
