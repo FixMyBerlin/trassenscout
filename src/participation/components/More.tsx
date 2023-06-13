031a45337a64f7878f9ee63a5d544765a39fe1cb
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-06-13 13:25:17 +0200

    restructure More, replace radio btns, update texts

--- src/participation/components/More.tsx
+++ src/participation/components/More.tsx
@@ -1,11 +1,8 @@
-import { useCallback, useState } from "react"
-import { Response } from "../data/types"
-import { ParticipationButton } from "./core/buttons/ParticipationButton"
-import { ScreenHeaderParticipation } from "./layout/ScreenHeaderParticipation"
 import { ParticipationH2 } from "./core/Text"
-import { ParticipationLabeledRadiobuttonGroup } from "./form/ParticipationLabeledRadiobuttonGroup"
-import SurveyForm from "./form/SurveyForm"
+import { ParticipationButton } from "./core/buttons/ParticipationButton"
 import { ParticipationButtonWrapper } from "./core/buttons/ParticipationButtonWrapper"
+import SurveyForm from "./form/SurveyForm"
+import { ScreenHeaderParticipation } from "./layout/ScreenHeaderParticipation"
 
 export { FORM_ERROR } from "src/core/components/forms"
 
@@ -16,42 +13,21 @@ type Props = {
 }
 
 export const More: React.FC<Props> = ({ more, onClickMore, onClickFinish }) => {
-  // It would be much nicer to useFormContext and formState.isDirty - but then this component has to be wrapped in Form (and FormProvider - tbd)
-  const [isDirty, setIsDirty] = useState(false)
-  const [isFeedback, setIsFeedback] = useState(false)
-
   const { title, description, questions, buttons } = more.pages[0]
   const question = questions[0]
-  const button = buttons[0]
-
-  const handleChange = useCallback(
-    (values: Record<string, any>) => {
-      setIsFeedback(Number(values.feedback) === question.props.responses[0].id)
-      setIsDirty(
-        Number(values.feedback) === question.props.responses[0].id ||
-          Number(values.feedback) === question.props.responses[1].id
-      )
-    },
-    [question.props.responses]
-  )
 
   return (
-    <SurveyForm onSubmit={onClickFinish} onChangeValues={handleChange}>
+    <>
       <ScreenHeaderParticipation title={title.de} description={description.de} />
       <ParticipationH2>{question.label.de}</ParticipationH2>
-      <ParticipationLabeledRadiobuttonGroup
-        items={question.props.responses.map((item: Response) => ({
-          scope: "feedback",
-          name: String(item.id),
-          label: item.text.de,
-          value: item.id,
-        }))}
-      />
       <ParticipationButtonWrapper>
-        <ParticipationButton disabled={!isDirty} onClick={isFeedback ? onClickMore : onClickFinish}>
-          {button.label.de}
+        <ParticipationButton color={buttons[0].color} onClick={onClickMore}>
+          {buttons[0].label.de}
+        </ParticipationButton>
+        <ParticipationButton color={buttons[1].color} onClick={onClickFinish}>
+          {buttons[1].label.de}
         </ParticipationButton>
       </ParticipationButtonWrapper>
-    </SurveyForm>
+    </>
   )
 }
