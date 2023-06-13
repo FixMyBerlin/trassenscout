528e36af6bd4fcdcba5827921d56440ee4dca110
Sven König <sven@fixmycity.de>
2023-06-13 16:27:01 +0200

    fix mailjet widget

--- src/participation/components/Email.tsx
+++ src/participation/components/Email.tsx
@@ -1,14 +1,11 @@
+import { useCallback, useEffect, useState } from "react"
+import { iframeResizer } from "iframe-resizer"
+
 export { FORM_ERROR } from "src/core/components/forms"
-import { useCallback, useState } from "react"
 import { ParticipationH2, ParticipationP } from "./core/Text"
-import { ParticipationButton } from "./core/buttons/ParticipationButton"
-import { ParticipationButtonWrapper } from "./core/buttons/ParticipationButtonWrapper"
 import { ParticipationLink } from "./core/links/ParticipationLink"
-import { ParticipationLabeledCheckbox } from "./form/ParticipationLabeledCheckbox"
-import { ParticipationLabeledTextField } from "./form/ParticipationLabeledTextField"
 import SurveyForm from "./form/SurveyForm"
 import { ScreenHeaderParticipation } from "./layout/ScreenHeaderParticipation"
-import clsx from "clsx"
 
 type Props = {
   onSubmit: any
@@ -28,6 +25,10 @@ export const Email: React.FC<Props> = ({ onSubmit, email }) => {
 
   const page = email.pages[0]
 
+  useEffect(() => {
+    iframeResizer({}, "#mailjet-widget")
+  }, [])
+
   return (
     <section>
       <SurveyForm onSubmit={handleSubmit} onChangeValues={handleChange}>
@@ -38,8 +39,7 @@ export const Email: React.FC<Props> = ({ onSubmit, email }) => {
         <div
           dangerouslySetInnerHTML={{
             __html: `
-              <iframe data-w-type="embedded" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://7p8q.mjt.lu/wgt/7p8q/t5g/form?c=f8dcc5f9" width="100%" style="height: 0;"></iframe>
-              <script type="text/javascript" src="https://app.mailjet.com/pas-nc-embedded-v1.js"></script>
+              <iframe id="mailjet-widget" data-w-type="embedded" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://7p8q.mjt.lu/wgt/7p8q/t5g/form?c=f8dcc5f9" width="100%" style="height: 0px;"></iframe>
           `,
           }}
         />
