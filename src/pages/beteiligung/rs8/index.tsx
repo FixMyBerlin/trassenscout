c6ed2a1edff9f8672fd8fcf67480ea6fd6c0ce6e
Tobias <tobias@fixmycity.de>
2023-06-14 09:32:09 +0200

    Code comment

--- src/pages/beteiligung/rs8/index.tsx
+++ src/pages/beteiligung/rs8/index.tsx
@@ -113,7 +113,7 @@ const ParticipationMainPage: BlitzPage = () => {
   let component
   switch (stage) {
     case "SURVEY":
-      // @ts-ignore
+      // @ts-ignore "Types of property 'version' are incompatible. / Type 'number' is not assignable to type '1'."
       component = <Survey survey={surveyDefinition} onSubmit={handleSubmitSurvey} />
       break
     case "FEEDBACK":
