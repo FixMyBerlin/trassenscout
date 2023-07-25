82837617d98a215fb4aa06ed68ce39235d06f7dc
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-07-25 17:58:49 +0200

    get and show grouped survey responses

---
+++ src/pages/[projectSlug]/surveys/[surveySlug].tsx
@@ -0,0 +1,44 @@
+import { BlitzPage, useParam } from "@blitzjs/next"
+import { usePaginatedQuery, useQuery } from "@blitzjs/rpc"
+import { useRouter } from "next/router"
+import { Suspense } from "react"
+import { PageHeader } from "src/core/components/pages/PageHeader"
+import { Spinner } from "src/core/components/Spinner"
+import { LayoutRs, MetaTags } from "src/core/layouts"
+import getSurveyResponses from "src/survey-responses/queries/getGroupedSurveyResponses"
+import getSurveyNew from "src/surveys/queries/getSurvey"
+
+export const SurveyResponseWithQuery = () => {
+  const router = useRouter()
+  const surveySlug = useParam("surveySlug", "string")
+  const projectSlug = useParam("projectSlug", "string")
+  const [survey] = useQuery(getSurveyNew, { slug: surveySlug })
+  const [{ surveyResponses, groupedSurveyResponses }] = usePaginatedQuery(getSurveyResponses, {
+    surveySlug,
+  })
+
+  return (
+    <>
+      <MetaTags noindex title={`Beteiligung ${survey.slug}`} />
+      <PageHeader title={`Beteiligung ${survey.slug}`} className="mt-12" />
+
+      <div>
+        <pre>{JSON.stringify(groupedSurveyResponses, null, 2)}</pre>
+      </div>
+    </>
+  )
+}
+
+const SurveyResponsePage: BlitzPage = () => {
+  return (
+    <LayoutRs>
+      <Suspense fallback={<Spinner page />}>
+        <SurveyResponseWithQuery />
+      </Suspense>
+    </LayoutRs>
+  )
+}
+
+SurveyResponsePage.authenticate = true
+
+export default SurveyResponsePage
---
+++ src/survey-responses/queries/getGroupedSurveyResponses.ts
@@ -0,0 +1,87 @@
+import { paginate } from "blitz"
+import { resolver } from "@blitzjs/rpc"
+import db, { Prisma } from "db"
+
+import { authorizeProjectAdmin } from "src/authorization"
+import getProjectIdBySlug from "src/projects/queries/getProjectIdBySlug"
+
+type GetSurveyResponsesInput = { surveySlug: string } & Pick<
+  Prisma.SurveyResponseFindManyArgs,
+  "where" | "orderBy" | "skip" | "take"
+>
+
+export default resolver.pipe(
+  // @ts-ignore
+  authorizeProjectAdmin(getProjectIdBySlug),
+  async ({
+    surveySlug,
+    where,
+    orderBy = { id: "desc" },
+    skip = 0,
+    take = 100,
+  }: GetSurveyResponsesInput) => {
+    // TODO at the moment surveyId is NOT the relation field
+    // const saveWhere = { survey: { slug: surveySlug }, ...where }
+    const {
+      items: surveyResponses,
+      hasMore,
+      nextPage,
+      count,
+    } = await paginate({
+      skip,
+      take,
+      count: () => db.surveyResponse.count({ where }),
+      query: (paginateArgs) =>
+        db.surveyResponse.findMany({
+          ...paginateArgs,
+          where: { surveyId: 1 }, // TODO at the moment we only want the first part of the survey and we get it by surveyId
+          orderBy,
+        }),
+    })
+
+    const groupedSurveyResponses: Record<string, Record<string, number>> = {}
+
+    if (surveyResponses.length) {
+      const responseObjectExample = JSON.parse(surveyResponses[0]!.data) as Record<
+        string,
+        number | number[]
+      >
+      Object.keys(responseObjectExample).forEach((responseKey) => {
+        let result: Record<number, number> = {}
+        surveyResponses.forEach((response) => {
+          const responseObject = JSON.parse(response.data) as Record<string, number>
+
+          if (typeof responseObject[responseKey] === "number") {
+            // @ts-ignore
+            if (responseObject[responseKey] in result) {
+              // @ts-ignore
+              result[responseObject[responseKey]] += 1
+            } else {
+              // @ts-ignore
+              result[responseObject[responseKey]] = 1
+            }
+          } else {
+            // @ts-ignore
+            responseObject[responseKey].forEach((responseKeyItem: number) => {
+              if (responseKeyItem in result) {
+                result[responseKeyItem] += 1
+              } else {
+                result[responseKeyItem] = 1
+              }
+            })
+          }
+        })
+
+        groupedSurveyResponses[responseKey] = result
+      })
+    }
+
+    return {
+      surveyResponses,
+      groupedSurveyResponses,
+      nextPage,
+      hasMore,
+      count,
+    }
+  },
+)
