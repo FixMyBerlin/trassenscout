ce38ca1bfec0995fe8dd1917b8f40aa0d105d635
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-08-08 17:57:24 +0200

    create table

--- src/pages/[projectSlug]/surveys/[surveyId]/responses/index.tsx
+++ src/pages/[projectSlug]/surveys/[surveyId]/responses/index.tsx
@@ -5,6 +5,7 @@ import { isFuture, isPast } from "date-fns"
 import { Suspense } from "react"
 import { SuperAdminBox } from "src/core/components/AdminBox"
 import { Spinner } from "src/core/components/Spinner"
+import { TableWrapper } from "src/core/components/Table/TableWrapper"
 import { Link } from "src/core/components/links"
 import { PageHeader } from "src/core/components/pages/PageHeader"
 import { H2 } from "src/core/components/text"
@@ -53,6 +54,63 @@ export const SurveyResponse = () => {
 
       <div className="space-y-4 mt-12">
         <H2>Kommentare aus Bürgerbeteiligung</H2>
+        <TableWrapper>
+          <table className="min-w-full divide-y divide-gray-300">
+            <thead className="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  className="opacity-0 py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-6"
+                >
+                  ID
+                </th>
+                <th
+                  scope="col"
+                  className="uppercase px-3 py-3.5 text-left text-sm font-semibold text-gray-600"
+                >
+                  Status
+                </th>
+                <th
+                  scope="col"
+                  className="uppercase px-3 py-3.5 text-left text-sm font-semibold text-gray-600"
+                >
+                  Baulastträger
+                </th>
+                <th
+                  scope="col"
+                  className="uppercase px-3 py-3.5 text-left text-sm font-semibold text-gray-600"
+                >
+                  Kommentare
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="divide-y divide-gray-200 bg-white">
+              {surveyResponsesFeedbackPart.map((response) => (
+                <tr key={response.id}>
+                  <td className="h-20 whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                    <div className="flex items-center font-medium text-gray-900">{response.id}</div>
+                  </td>
+                  <td className="h-20 whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                    <div className="flex items-center font-medium text-gray-900">
+                      {response.status}
+                    </div>
+                  </td>
+                  <td className="h-20 whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                    <div className="flex items-center font-medium text-gray-900">
+                      {response.operatorId || "kein Baulastträger zugeordnet"}
+                    </div>
+                  </td>
+                  <td className="py-4 pl-4 pr-3 text-sm sm:pl-6">
+                    <div className="font-medium text-gray-900 line-clamp-3">
+                      {JSON.parse(response.data)["34"] || JSON.parse(response.data)["35"]}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableWrapper>
         <code>{JSON.stringify(surveyResponsesFeedbackPart)}</code>
       </div>
     </>
