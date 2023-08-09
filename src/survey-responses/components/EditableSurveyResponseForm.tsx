e712fb196a6163282dc0ec33a1d4a98e3a069572
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-08-09 11:53:37 +0200

    add editableSurveyResponseForm with onChangeValues

---
+++ src/survey-responses/components/EditableSurveyResponseForm.tsx
@@ -0,0 +1,56 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import clsx from "clsx"
+import { PropsWithoutRef, ReactNode, useEffect } from "react"
+import { FormProvider, useForm, UseFormProps } from "react-hook-form"
+import { z } from "zod"
+
+export interface FormProps<S extends z.ZodType<any, any>>
+  extends Omit<PropsWithoutRef<JSX.IntrinsicElements["form"]>, "onSubmit"> {
+  children: ReactNode
+  schema?: S
+  onSubmit: (values: z.infer<S>) => void
+  onChangeValues: (values: any) => void
+  initialValues?: UseFormProps<z.infer<S>>["defaultValues"]
+}
+
+export const FORM_ERROR = "FORM_ERROR"
+
+export function EditableSurveyResponseForm<S extends z.ZodType<any, any>>({
+  children,
+  schema,
+  initialValues,
+  onSubmit,
+  onChangeValues,
+  className,
+  ...props
+}: FormProps<S>) {
+  const ctx = useForm<z.infer<S>>({
+    mode: "onBlur",
+    resolver: schema ? zodResolver(schema) : undefined,
+    defaultValues: initialValues,
+  })
+  useEffect(() => {
+    if (onChangeValues) {
+      onChangeValues(ctx.getValues())
+    }
+  }, [onChangeValues, ctx])
+
+  if (onChangeValues) props.onChange = () => onChangeValues(ctx.getValues())
+
+  return (
+    <FormProvider {...ctx}>
+      <form
+        className={className}
+        onSubmit={async (e: React.FormEvent<HTMLFormElement>) => {
+          await ctx.handleSubmit(async (values) => await onSubmit(values))()
+          e.preventDefault()
+        }}
+        {...props}
+      >
+        {children}
+      </form>
+    </FormProvider>
+  )
+}
+
+export default EditableSurveyResponseForm
--- src/survey-responses/components/EditableSurveyResponseListItem.tsx
+++ src/survey-responses/components/EditableSurveyResponseListItem.tsx
@@ -3,6 +3,10 @@ import { SurveyResponse } from "@prisma/client"
 import clsx from "clsx"
 import { useRouter } from "next/router"
 import { useRouterQuery } from "@blitzjs/next"
+import EditableSurveyResponseForm from "./EditableSurveyResponseForm"
+import { RadioIcon } from "@heroicons/react/24/solid"
+import { LabeledTextField } from "src/core/components/forms"
+import { H3 } from "src/core/components/text"
 export { FORM_ERROR } from "src/core/components/forms"
 
 type Props = {
@@ -29,51 +33,67 @@ const EditableSurveyResponseListItem: React.FC<Props> = ({ response, columnWidth
   const responseDetails = parseInt(String(params.responseDetails))
   const open = response.id === Number(responseDetails)
   return (
-    <Disclosure
-      open={open}
-      onOpen={handleOpen}
-      onClose={handleClose}
-      classNameButton="py-4 text-left text-sm text-gray-900 hover:bg-gray-50"
-      classNamePanel="flex flex-start"
-      button={
-        <>
-          <div
-            className={clsx(
-              columnWidthClasses.id,
-              "flex-shrink-0 h-20 whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6",
-            )}
-          >
-            <div className="flex items-center font-medium text-gray-900">{response.id}</div>
-          </div>
-          <div
-            className={clsx(
-              columnWidthClasses.status,
-              "flex-shrink-0 h-20 whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6",
-            )}
-          >
-            <div className="flex items-center font-medium text-gray-900">{response.status}</div>
-          </div>
-          <div
-            className={clsx(
-              columnWidthClasses.operator,
-              "flex-shrink-0 h-20 whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6",
-            )}
-          >
-            <div className="flex items-center font-medium text-gray-900">
-              {response.operatorId || "k.A."}
-            </div>
-          </div>
-          <div className="flex-grow py-4 pl-4 pr-3 text-sm sm:pl-6">
-            <div className={clsx(open || "line-clamp-3", "font-medium items-center text-gray-900")}>
-              {/* @ts-ignore */}
-              {JSON.parse(response.data)["34"] || JSON.parse(response.data)["35"]}
-            </div>
-          </div>
-        </>
-      }
+    <EditableSurveyResponseForm
+      onChangeValues={() => console.log("change success")}
+      onSubmit={() => console.log("success")}
     >
-      <div className="ml-3 px-3 pb-2 pt-6 sm:ml-64">TODO</div>
-    </Disclosure>
+      <Disclosure
+        open={open}
+        onOpen={handleOpen}
+        onClose={handleClose}
+        classNameButton="py-4 text-left text-sm text-gray-900 hover:bg-gray-50"
+        classNamePanel="flex flex-start"
+        button={
+          <>
+            <div
+              className={clsx(
+                columnWidthClasses.id,
+                "flex-shrink-0 h-20 whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6",
+              )}
+            >
+              <div className="flex items-center font-medium text-gray-900">{response.id}</div>
+            </div>
+            <div
+              className={clsx(
+                columnWidthClasses.status,
+                "flex-shrink-0 h-20 whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6",
+              )}
+            >
+              <div className="flex items-center font-medium text-gray-900">{response.status}</div>
+            </div>
+            <div
+              className={clsx(
+                columnWidthClasses.operator,
+                "flex-shrink-0 h-20 whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6",
+              )}
+            >
+              <div className="flex items-center font-medium text-gray-900">
+                {response.operatorId || "k.A."}
+              </div>
+            </div>
+            <div className="flex-grow py-4 pl-4 pr-3 text-sm sm:pl-6">
+              <div
+                className={clsx(open || "line-clamp-3", "font-medium items-center text-gray-900")}
+              >
+                {/* @ts-ignore */}
+                {JSON.parse(response.data)["34"] || JSON.parse(response.data)["35"]}
+              </div>
+            </div>
+          </>
+        }
+      >
+        <div className="ml-3 px-3 pb-2 pt-6 sm:ml-64">
+          <H3>example</H3>
+          <LabeledTextField
+            type="email"
+            name="email"
+            label="E-Mail-Adresse"
+            placeholder="name@beispiel.de"
+            autoComplete="email"
+          />
+        </div>
+      </Disclosure>
+    </EditableSurveyResponseForm>
   )
 }
 
