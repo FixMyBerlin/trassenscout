888b9df3ce247e45378ef584f2290ae563053fc2
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-08-18 11:38:50 +0200

    fix export

--- src/survey-responses/components/analysis/PieChart.tsx
+++ src/survey-responses/components/analysis/PieChart.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"]
 
-const PieChart: React.FC<Props> = ({ data }) => {
+export const PieChart: React.FC<Props> = ({ data }) => {
   return (
     <>
       <ResponsiveContainer width="100%" height="100%">
