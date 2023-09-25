2e5e22ed5a7639135bf7920e6e568a18688f85e8
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-09-25 17:32:15 +0200

    Form Error: update prisma error message translations

--- src/core/components/forms/errorMessageTranslations.ts
+++ src/core/components/forms/errorMessageTranslations.ts
@@ -1,15 +1,16 @@
 type TranslatedMessages = { [key: string]: string }
 
 export const errorMessageTranslations: TranslatedMessages = {
-  "Error: Invalid `prisma.contact.create()` invocation:Unique constraint failed on the fields: (`email`)":
-    "Diese E-Mail-Adresse wird bereits vergeben. Eine E-Mail-Adresse darf nur einem Kontakt zugewiesen werden.",
-  "Error: Invalid `prisma.project.update()` invocation:Unique constraint failed on the fields: (`slug`)":
-    "Dieses URL-Segment wird bereits vergeben. Eine URL-Segment darf nur einmalig zugewiesen werden.",
-  "Error: Invalid `prisma.section.update()` invocation:Unique constraint failed on the fields: (`projectId`,`order`)":
+  "PrismaClientKnownRequestError: Invalid `prisma.contact.create()` invocation:Unique constraint failed on the fields: (`projectId`,`email`)":
+    "Diese E-Mail-Adresse ist bereits vergeben. Eine E-Mail-Adresse darf nur einem Kontakt zugewiesen werden",
+  "PrismaClientKnownRequestError: Invalid `prisma.project.create()` invocation:Unique constraint failed on the fields: (`slug`)":
+    "Dieses URL-Segment ist bereits für eine andere Trasse vergeben. Ein URL-Segment darf nur einmalig zugewiesen werden.",
+  "PrismaClientKnownRequestError: Invalid `prisma.subsection.update()` invocation:Unique constraint failed on the fields: (`projectId`,`order`)":
     "Der Wert für 'Reihenfolge' wird bereits von einer anderen Teilstrecke verwendet. Bitte wählen Sie einen anderen Wert; er kann auch viel höher sein wie beispielweise 50.",
-  "Error: Invalid `prisma.subsection.update()` invocation:Unique constraint failed on the fields: (`sectionId`,`order`)":
-    "Der Wert für 'Reihenfolge' wird bereits von einem anderen Planungsabschnitt verwendet. Bitte wählen Sie einen anderen Wert; er kann auch viel höher sein wie beispielweise 50.",
-  "Error: Invalid `prisma.subsubsection.update()` invocation:Unique constraint failed on the fields: (`subsectionId`,`order`)":
-    "Der Wert für 'Reihenfolge' wird bereits von einer anderen Führung verwendet. Bitte wählen Sie einen anderen Wert; er kann auch viel höher sein wie beispielweise 50.",
+  "PrismaClientKnownRequestError: Invalid `prisma.subsubsection.create()` invocation:Unique constraint failed on the fields: (`subsectionId`,`slug`)":
+    "Dieses URL-Segment ist bereits vergeben. Ein URL-Segment darf nur einmalig pro Planungsabschnitt zugewiesen werden.",
+  "PrismaClientKnownRequestError: Invalid `prisma.subsubsection.update()` invocation:Unique constraint failed on the fields: (`subsectionId`,`slug`)":
+    "Dieses URL-Segment ist bereits vergeben. Ein URL-Segment darf nur einmalig pro Planungsabschnitt zugewiesen werden.",
+
   // "Dies ist keine gültige E-Mail-Adresse.",
 }
