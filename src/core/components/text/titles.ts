7e9cf601680af62328e929139ba76d1880b40e6f
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-10-25 15:13:03 +0200

    clean up

--- src/core/components/text/titles.ts
+++ src/core/components/text/titles.ts
@@ -4,8 +4,6 @@ export const shortTitle = (slug: string) => {
 
 export const longTitle = (slug: string) => {
   return slug
-    .replace("rs", "Radschnellverbindung ") // Project
-    .replace("ts", "Teilstrecke ") // Section
     .replace("pa", "Planungsabschnitt ") // Subsection
     .replace("rf", "Regelführung ") // Subsubsection
     .replace("sf", "Sonderführung ") // Subsubsection
