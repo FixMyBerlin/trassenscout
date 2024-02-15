0eac4827f703fd00ed31933ba79e9989473342e2
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2024-02-15 12:14:56 +0100

    SurveyResponse: size chart depending on the length of answer options

--- src/survey-responses/components/analysis/GroupedSurveyResponseItem.tsx
+++ src/survey-responses/components/analysis/GroupedSurveyResponseItem.tsx
@@ -1,6 +1,6 @@
+import clsx from "clsx"
 import { H3 } from "src/core/components/text"
 import { BarChart } from "./BarChart"
-import { PieChart } from "./PieChart"
 export { FORM_ERROR } from "src/core/components/forms"
 
 type Props = {
@@ -14,13 +14,31 @@ export const GroupedSurveyResponseItem: React.FC<Props> = ({
   responseData,
   chartType,
 }) => {
-  if (!responseData) return
+  if (!responseData?.length) return
 
+  let heightClass: string
+
+  switch (true) {
+    case responseData.length <= 3:
+      heightClass = "h-[220px]"
+      break
+    case responseData.length <= 4:
+      heightClass = "h-[290px]"
+      break
+    case responseData.length <= 6:
+      heightClass = "h-[300px]"
+      break
+    case responseData.length <= 8:
+      heightClass = "h-[600px]"
+      break
+    default:
+      heightClass = "h-[700px]"
+  }
   return (
     <div className="border rounded  py-3.5">
       {questionLabel && <H3 className="border-b pb-3.5 px-3.5">{questionLabel}</H3>}
 
-      <div className="h-[350px] px-3.5">
+      <div className={clsx("h-[480px] px-3.5", heightClass)}>
         {chartType === "bar" && <BarChart data={responseData} />}
         {/* <PieChart data={responseData} /> */}
       </div>
