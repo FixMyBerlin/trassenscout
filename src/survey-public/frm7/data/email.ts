542207d2a910633d9860f30b444637ec7c9f0a4b
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2024-02-15 10:21:29 +0100

    Survey: update texts frm7

--- src/survey-public/frm7/data/email.ts
+++ src/survey-public/frm7/data/email.ts
@@ -4,7 +4,7 @@ export const emailDefinition: TEmail = {
   title: { de: "Vielen Dank für Ihre Teilnahme!" },
   questionText: { de: "Was passiert als Nächstes?" },
   description: {
-    de: "Nach Abschluss der Beteiligung (31.01.2024) werden Ihre Anregungen vom Planungsteam ausgewertet. Dabei wird geprüft, ob und inwieweit Ihre Hinweise in die weitere Planung einbezogen werden können. Bitte haben Sie Verständnis dafür, dass wir nicht jeden Hinweis kommentieren können. Nach der Auswertung werden wir zusammenfassend zu den genannten Themen Rückmeldung geben.",
+    de: "Nach Abschluss der Beteiligung (31.03.2024) werden Ihre Anregungen vom Planungsteam ausgewertet. Dabei wird geprüft, ob und inwieweit Ihre Hinweise in die weitere Planung einbezogen werden können. Bitte haben Sie Verständnis dafür, dass wir nicht jeden Hinweis kommentieren können. Nach der Auswertung werden wir zusammenfassend zu den genannten Themen Rückmeldung geben.",
   },
   mailjetWidgetUrl: "https://7p8q.mjt.lu/wgt/7p8q/xwjs/form?c=84250207",
   button: {
