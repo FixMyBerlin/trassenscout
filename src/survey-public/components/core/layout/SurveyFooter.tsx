7ffdc17620587a67862aff2abbc8393d24f4c448
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2024-02-15 10:20:14 +0100

    Survey: duplicate footer component and make links open in new window

--- src/survey-public/components/core/layout/SurveyFooter.tsx
+++ src/survey-public/components/core/layout/SurveyFooter.tsx
@@ -1,13 +1,19 @@
+import { Routes } from "@blitzjs/next"
 import React from "react"
-import { FooterLinkList } from "../../../../core/layouts/Footer/FooterLinkList"
-import { links } from "../../../../core/layouts/Footer/links.const"
+import { SurveyFooterLinkList } from "./SurveyFooterLinkList"
 
 export const SurveyFooter: React.FC = () => {
   return (
     <footer className="z-0 bg-gray-100 px-6 py-8">
       <div className="pt-6">
         <div className="flex flex-row justify-between">
-          <FooterLinkList linkList={links} className="ml-auto mr-4 flex-none sm:pr-[12vw]" />
+          <SurveyFooterLinkList
+            linkList={[
+              { name: "Kontakt & Impressum", href: Routes.Kontakt(), blank: true },
+              { name: "Datenschutz", href: Routes.Datenschutz(), blank: true },
+            ]}
+            className="ml-auto mr-4 flex-none sm:pr-[12vw]"
+          />
         </div>
       </div>
     </footer>
---
+++ src/survey-public/components/core/layout/SurveyFooterLinkList.tsx
@@ -0,0 +1,36 @@
+import { RouteUrlObject } from "blitz"
+import clsx from "clsx"
+import React from "react"
+import { Link } from "src/core/components/links"
+
+type TSurveyFooterLink = {
+  name: string
+  href: RouteUrlObject | string
+  blank: boolean
+}
+
+type Props = {
+  linkList: TSurveyFooterLink[]
+  className?: string
+}
+
+export const SurveyFooterLinkList: React.FC<Props> = ({ linkList, className }) => {
+  return (
+    <div className="space-y-3">
+      <p className="text-sm font-semibold text-gray-400">RECHTLICHES</p>
+      <ul className={clsx("space-y-3", className)}>
+        {linkList.map((item) => (
+          <li key={item.name}>
+            <Link
+              href={item.href}
+              blank={item.blank}
+              classNameOverwrites="text-gray-400 text-sm hover:underline"
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
