39df451f429d760cc00dcbeed80d448ba8b6e1e4
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2024-07-03 15:55:52 +0200

    Survey/Admin: show all responses of a survey in admin ui

--- src/survey-responses/queries/getSurveySurveyResponses.ts
+++ src/survey-responses/queries/getSurveySurveyResponses.ts
@@ -11,27 +11,16 @@ export default resolver.pipe(
   async ({ projectSlug, surveyId }: GetSurveySessionsWithResponsesInput) => {
     const rawSurveyResponse = await db.surveyResponse.findMany({
       where: {
-        // Only surveyResponse.session.project === projectSlug
         surveySession: {
           survey: { project: { slug: projectSlug } },
-          // Only surveyResponse.surveyId === surveyId
           surveyId,
         },
-        // Only surveyResponse.surveyPart === 2
-        // the field here just represents first or second part of the survey json
-        // surveyPart `2` in feedback.ts
-        surveyPart: 2,
+
+        surveyPart: 1,
       },
       orderBy: { id: "desc" },
       include: {
-        operator: { select: { id: true, title: true, slug: true } },
-        // surveyResponseTopics: {
-        //   include: {
-        //     surveyResponseTopic: { select: { id: true, title: true } },
-        //   },
-        // },
-        surveyResponseTopics: true,
-        surveySession: { select: { createdAt: true, id: true } },
+        surveySession: true,
       },
     })
 
@@ -39,10 +28,8 @@ export default resolver.pipe(
       // Make `data` an object to work with…
       .map((response) => {
         const data = JSON.parse(response.data)
-        const surveyResponseTopics = response.surveyResponseTopics.map(
-          (topic) => topic.surveyResponseTopicId,
-        )
-        return { ...response, data, surveyResponseTopics }
+
+        return { ...response, data }
       })
       // Sometimes the fronted received a different order for unknown reasons
       .sort((a, b) => b.id - a.id)
