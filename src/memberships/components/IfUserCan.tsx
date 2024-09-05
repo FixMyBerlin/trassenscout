c7411143337b730753a64b1e3e47d5cd0769ede0
Sven König <sven@de-selby.com>
2024-09-05 10:03:31 +0200

    add border to <IfUserCan*> for debugging

--- src/memberships/components/IfUserCan.tsx
+++ src/memberships/components/IfUserCan.tsx
@@ -1,14 +1,20 @@
 import React from "react"
 import { useUserCan } from "../hooks/useUserCan"
 
+const BORDER = false
+
 type Props = {
   children: React.ReactNode
 }
 
+export const Border: React.FC<Props> = ({ children }) => {
+  return BORDER ? <div className="border-4 border-red-500">{children}</div> : children
+}
+
 export const IfUserCanView: React.FC<Props> = ({ children }) => {
-  return useUserCan().view ? children : null
+  return useUserCan().view ? <Border>{children}</Border> : null
 }
 
 export const IfUserCanEdit: React.FC<Props> = ({ children }) => {
-  return useUserCan().edit ? children : null
+  return useUserCan().edit ? <Border>{children}</Border> : null
 }
