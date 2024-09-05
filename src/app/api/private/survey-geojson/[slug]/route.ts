f4a0593c7e35ee3510fc7242fcd4c0cf28b67f1a
Tobias <tobias@fixmycity.de>
2024-09-05 14:03:39 +0200

    Survey API: Extend API results
    We need additional data to show in Atlas

--- src/app/api/private/survey-geojson/[slug]/route.ts
+++ src/app/api/private/survey-geojson/[slug]/route.ts
@@ -1,8 +1,10 @@
+import { Feature, LineString, Point, Position } from "@turf/helpers"
 import adler32 from "adler-32"
 import db from "db"
 import { feedbackDefinition } from "src/survey-public/radnetz-brandenburg/data/feedback"
 
 const categories = Object.fromEntries(
+  // todo survey clean up after survey BB: remove or find generic way to do this
   // @ts-expect-error
   feedbackDefinition.pages[0]?.questions
     .find((q) => q.id == 22)
@@ -20,8 +22,6 @@ export async function GET(request: Request, { params }: { params: { slug: string
   const searchParams = new URL(request.url).searchParams
   // to not return geojson but debugging data
   const debug = searchParams.has("debug")
-  // for debugging - simplify lines to make output more readable
-  const simplify = searchParams.has("simplify")
 
   if (slug !== "radnetz-brandenburg") {
     // for any other survey than 'radnetz-brandenburg' this route will not work
@@ -34,12 +34,14 @@ export async function GET(request: Request, { params }: { params: { slug: string
       SurveySession: {
         select: {
           responses: {
-            where: { surveyPart: 2 },
             select: {
               id: true,
               data: true,
+              surveyPart: true,
+              surveySessionId: true,
             },
           },
+          createdAt: true,
         },
       },
     },
@@ -49,38 +51,44 @@ export async function GET(request: Request, { params }: { params: { slug: string
     return Response.json({ statusText: "Not Found" }, { status: 404 })
   }
 
-  survey.SurveySession.forEach((session) => {
-    session.responses.forEach((response) => {
-      // @ts-expect-error
-      // prettier-ignore
-      const { 20: lineId, 21: lineGeometry, 22: categoryId, 24: location, 25: text } = JSON.parse(response.data)
-      const data = {
-        lineId,
-        lineGeometry,
-        category: categories[categoryId],
-        location,
-        text,
-      }
-      if (data.lineGeometry) {
-        data.lineGeometry = JSON.parse(data.lineGeometry)
-        if (simplify) {
-          data.lineGeometry = [data.lineGeometry[0], data.lineGeometry.slice(-1)[0]]
-        }
-      }
-      response.data = data as any
-    })
-    // @ts-expect-error
-    session.responses = session.responses.filter((response) => !!response.data.lineGeometry)
-  })
+  const results = survey.SurveySession.map((session) => {
+    return session.responses
+      .filter((survey) => survey.surveyPart === 2)
+      .map((response) => {
+        const rawData: any = JSON.parse(response.data)
 
-  const features = survey.SurveySession.map((session) =>
-    // @ts-expect-error
-    session.responses.map((response) => ({ reponseId: response.id, ...response.data })),
-  )
-    .flat()
-    .map((data: any) => {
-      const { reponseId, lineId, lineGeometry, category, location, text } = data
-      const features = [
+        // Some data is stored on part 1 of the survey
+        const rawPart1 = session.responses.find(
+          (r) => r.surveyPart === 1 && r.surveySessionId === response.surveySessionId,
+        )?.data
+        const part1: any = rawPart1 ? JSON.parse(rawPart1) : undefined
+
+        // todo survey clean up after survey BB: remove BB specific fields
+        return {
+          reponseId: response.id,
+          sessionCreatedAt: session.createdAt,
+          Author: `${part1?.[1]} ${part1?.[2]}` as string,
+          Institut: part1?.[5] as string,
+          Landkreis: part1?.[6] as string,
+          lineId: rawData[20] as string, // "165-89"
+          lineGeometry: rawData[21]
+            ? (JSON.parse(rawData[21]) as LineString["coordinates"])
+            : undefined,
+          category: categories[rawData[22]],
+          location: rawData[24] as null | { lng: number; lat: number },
+          text: rawData[25] as string,
+        }
+      })
+      .filter((d) => d.lineGeometry !== undefined)
+  }).flat()
+
+  // return Response.json({ results: results.filter((r) => r.reponseId === 1109) })
+
+  const features = results
+    .map(({ lineGeometry, location, ...result }) => {
+      if (lineGeometry === undefined) return
+
+      const features: (Feature<LineString> | Feature<Point>)[] = [
         {
           type: "Feature",
           geometry: {
@@ -92,32 +100,28 @@ export async function GET(request: Request, { params }: { params: { slug: string
           // Plan B would be to set source.promoteId which can also be a string.
           // Docs: https://maplibre.org/maplibre-style-spec/expressions/#feature-state
           // The method we use here is the same we use in atlas `transformFile.ts`
-          id: new Uint32Array([adler32.str(JSON.stringify(`line-${reponseId}-${lineId}`))])[0]!,
+          id: new Uint32Array([adler32.str(`line-${result.reponseId}-${result.lineId}`)])[0]!,
           properties: {
-            featureId: `line-${reponseId}-${lineId}`,
-            id: reponseId,
-            Thema: category,
-            Hinweis: text,
+            ...result,
+            precision: location ? "point" : "line",
             geometryType: "line",
-            geometryLineId: lineId,
           },
         },
       ]
+      // Each feedback is stored on the line geometry, to make it easy to select all at once in Atlas.
+      // Point data is added in addition to see the precise location.
       if (location) {
         features.push({
           type: "Feature",
           geometry: {
             type: "Point",
-            coordinates: [location.lng, location.lat],
+            coordinates: [location.lng, location.lat] as Position,
           },
-          id: new Uint32Array([adler32.str(JSON.stringify(`point-${reponseId}-${lineId}`))])[0]!,
+          id: new Uint32Array([adler32.str(`point-${result.reponseId}-${result.lineId}`)])[0]!,
           properties: {
-            featureId: `point-${reponseId}-${lineId}`,
-            id: reponseId,
-            Thema: category,
-            Hinweis: text,
+            ...result,
+            precision: location ? "point" : "line",
             geometryType: "point",
-            geometryLineId: lineId,
           },
         })
       }
