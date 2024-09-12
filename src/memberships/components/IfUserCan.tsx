a23cf3dcf5959a28e619272c0c97242e36a2c93d
Tobias <tobias@fixmycity.de>
2024-09-12 09:53:17 +0200

    Roles: Improve admin hint to see where UI is hidden for roles

--- src/memberships/components/IfUserCan.tsx
+++ src/memberships/components/IfUserCan.tsx
@@ -1,19 +1,35 @@
+import { useSession } from "@blitzjs/auth"
+import { NoSymbolIcon } from "@heroicons/react/20/solid"
+import { isProduction } from "src/core/utils"
 import { useUserCan } from "../hooks/useUserCan"
-
-const BORDER = false
 
 type Props = {
   children: React.ReactNode
 }
 
-export const Border = ({ children }: Props) => {
-  return BORDER ? <div className="border-4 border-red-500">{children}</div> : children
+// This helps Admins to see which areas are only visible to permission holders
+export const AdminHint = ({ children }: Props) => {
+  const session = useSession()
+  if (!isProduction && session.role === "ADMIN") {
+    return (
+      <>
+        <span
+          className="m-1 inline-block rounded border border-purple-300 bg-purple-100 p-1"
+          title="An dieser Stelle erscheint ein UI Element nur für Nutzer, die bestimmte Rechte haben."
+        >
+          <NoSymbolIcon className="h-4 w-4 text-purple-700" />
+        </span>
+        {children}
+      </>
+    )
+  }
+  return children
 }
 
-export const IfUserCanView = ({ children }: Props) => {
-  return useUserCan().view ? <Border>{children}</Border> : null
-}
+// export const IfUserCanView = ({ children }: Props) => {
+//   return useUserCan().view ? <AdminHint>{children}</AdminHint> : null
+// }
 
 export const IfUserCanEdit = ({ children }: Props) => {
-  return useUserCan().edit ? <Border>{children}</Border> : null
+  return useUserCan().edit ? <AdminHint>{children}</AdminHint> : null
 }
