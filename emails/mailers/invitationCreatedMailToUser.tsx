4984b311334900719d832fdbecaf3ea20e6fc05c
Noemique <152984460+Noemique@users.noreply.github.com>
2024-09-25 18:58:45 +0200

    Update invitationCreatedMailToUser.tsx

--- emails/mailers/invitationCreatedMailToUser.tsx
+++ emails/mailers/invitationCreatedMailToUser.tsx
@@ -17,20 +17,18 @@ export async function invitationCreatedMailToUser(props: Props) {
   const introMarkdown = `
 Guten Tag!
 
-# Sie wurden eingeladen am Projekt ${quote(props.projectName)} mitzuwirken
-
-Diese Einladung wurde von ${props.inviterName} verschickt.
+# ${props.inviterName} hat Sie soeben eingeladen, am Projekt ${quote(props.projectName)} mitzuwirken.
 
 Bitte registieren Sie sich, um die Einladung anzunehmen.`
 
   const outroMarkdown = `
-Wenn Sie schon einen Account unter diese E-Mail-Adresse haben, [melden Sie sich bitte damit an]( ${mailUrl(props.loginPath)} ), um die Einladung anzunehmen.
+Falls Sie schon einen Trassenscout-Account unter dieser E-Mail-Adresse besitzen, [melden Sie sich bitte damit an]( ${mailUrl(props.loginPath)} ), um die Einladung anzunehmen.
 `
 
   const message: Mail = {
     From: addressNoreply,
     To: [{ Email: props.userEmail }],
-    Subject: `Trassenscout: Einladung zum Projekt ${quote(props.projectName)}`,
+    Subject: `Trassenscout: Ihre Einladung zum Projekt ${quote(props.projectName)}`,
     introMarkdown,
     ctaLink: mailUrl(props.signupPath),
     ctaText: "Einladung annehmen und registrieren",
