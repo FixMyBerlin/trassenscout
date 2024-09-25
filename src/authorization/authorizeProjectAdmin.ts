eeb490afac818716dee2163b9c4f3108a92983f2
Tobias <tobias@fixmycity.de>
2024-09-25 17:07:40 +0200

    Roles: Strong membership check

--- src/authorization/authorizeProjectAdmin.ts
+++ src/authorization/authorizeProjectAdmin.ts
@@ -1,6 +1,6 @@
-import { UserRoleEnum } from "@/db"
+import db, { UserRoleEnum } from "@/db"
 import { SessionContext } from "@blitzjs/auth"
-import { AuthorizationError } from "blitz"
+import { AuthorizationError, NotFoundError } from "blitz"
 import { editorRoles } from "./constants"
 import { MembershipRole } from "./types"
 
@@ -29,18 +29,24 @@ export function authorizeProjectAdmin(
 
     // check if user has project memberships and one of the required roles
     const idOrSlug = await getProjectIdOrSlug(input)
-
     if (!idOrSlug) {
       throw new Error("Invalid idOrSlug")
     }
 
-    if (
-      !!ctx.session.memberships!.find(
-        ({ project, role }: any) =>
-          (Number.isFinite(idOrSlug) ? project.id === idOrSlug : project.slug === idOrSlug) &&
-          requiredRoles.includes(role),
-      )
-    ) {
+    const projectId =
+      typeof idOrSlug === "string"
+        ? (await db.project.findUnique({ where: { slug: idOrSlug }, select: { id: true } }))?.id
+        : idOrSlug
+    if (!projectId) throw new NotFoundError()
+
+    // We want to make extra sure that we look at the latest role data.
+    // Which is why we don't use the session to lookup the role.
+    // Side note: We do delete the session whenever we change a users role, so this might be overkill?
+    const freshMembership = await db.membership.findUnique({
+      where: { projectId_userId: { userId: ctx.session.userId, projectId } },
+    })
+    if (!freshMembership) throw new AuthorizationError()
+    if (requiredRoles.includes(freshMembership.role)) {
       return input
     }
 
