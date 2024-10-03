e13406e7ee29422f56fb281554c13ede4b11e51c
Tobias <tobias@fixmycity.de>
2024-10-03 21:32:03 +0200

    Layout: Disable Matomo in app directory because it does not work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,13 @@ import { Metadata, Viewport } from "next"
 import { BlitzProvider } from "../blitz-client"
 import { defaultMetadata } from "./_components/layouts/defaultMetadata"
 import { fontClasses } from "./_components/layouts/fonts"
-import { useMatomo } from "./_components/layouts/useMatomo"
 import { TailwindResponsiveHelper } from "./_components/TailwindResponsiveHelper/TailwindResponsiveHelper"
 
 export const metadata: Metadata = defaultMetadata
 export const viewport: Viewport = { themeColor: "#1f2937" }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  useMatomo()
+  // useMatomo()
 
   return (
     <html lang="de" className={clsx(fontClasses, "h-full scroll-smooth")}>
