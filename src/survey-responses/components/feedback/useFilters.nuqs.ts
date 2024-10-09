3efbf854f0bce6662e6c17576f8f7710cae94ba8
Tobias <tobias@fixmycity.de>
2024-10-09 11:21:04 +0200

    Filters: Expand on dynamic zod schema for future reference

--- src/survey-responses/components/feedback/useFilters.nuqs.ts
+++ src/survey-responses/components/feedback/useFilters.nuqs.ts
@@ -12,7 +12,13 @@ export const filterSchema = z
     topics: z.array(z.string()),
     searchterm: z.string(),
   })
-  .passthrough() // Allow additional key-value pairs (additional filters)
+  // Allow additional key-value pairs (additional filters) as optionally specified by TBackendConfig['additionalFilters']
+  // Note: We could try to improve the type safety for those properties with dynamic zod types
+  //       But those are not straight forward. Some good reading is…
+  //       - https://dev.to/yanagisawahidetoshi/dynamically-modifying-validation-schemas-in-zod-a-typescript-and-react-hook-form-example-3ho0
+  //       - https://github.com/colinhacks/zod/discussions/3287#discussioncomment-8631944
+  //       - https://stackoverflow.com/questions/75984188/zod-how-to-dynamically-generate-a-schema
+  .passthrough()
 
 export type FilterSchema = z.infer<typeof filterSchema>
 
