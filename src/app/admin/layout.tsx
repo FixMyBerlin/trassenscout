d042cb44ae96e71fa410dbf27ce4204e9162f17b
Tobias <tobias@fixmycity.de>
2024-10-15 08:15:40 +0200

    Admin: Show current user for reference

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,5 @@
-import { useAuthenticatedBlitzContext } from "@/src/blitz-server"
+import { getBlitzContext, useAuthenticatedBlitzContext } from "@/src/blitz-server"
+import getCurrentUser from "@/src/server/users/queries/getCurrentUser"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -11,11 +12,17 @@ export const metadata: Metadata = {
 
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
   await useAuthenticatedBlitzContext({ role: ["ADMIN"], redirectTo: "/auth/login" })
+  const ctx = await getBlitzContext()
+  const user = await getCurrentUser(null, ctx)
 
   return (
     <div className="relative flex h-full flex-col bg-pink-50">
       <main className="prose prose-pink mx-auto w-full max-w-4xl py-10 prose-hr:border-white">
         <h1 className="text-pink-600">Trassenscout ADMIN</h1>
+        <details>
+          <summary>Current user</summary>
+          <pre>{JSON.stringify(user, undefined, 2)}</pre>
+        </details>
         {children}
       </main>
     </div>
