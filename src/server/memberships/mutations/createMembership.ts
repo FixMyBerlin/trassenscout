26b8290302c9063590f9a718d84ff0beaddbdaaf
Tobias <tobias@fixmycity.de>
2024-10-15 08:15:30 +0200

    Roles: Fix membership create for admins

--- src/server/memberships/mutations/createMembership.ts
+++ src/server/memberships/mutations/createMembership.ts
@@ -1,5 +1,6 @@
 import db from "@/db"
 import { resolver } from "@blitzjs/rpc"
+import { selectUserFieldsForSession } from "../../auth/shared/selectUserFieldsForSession"
 import { MembershipSchema } from "../schema"
 
 export const CreateMembershipSchema = MembershipSchema
@@ -7,11 +8,19 @@ export const CreateMembershipSchema = MembershipSchema
 export default resolver.pipe(
   resolver.zod(CreateMembershipSchema),
   resolver.authorize("ADMIN"),
-  async (input) => {
+  async (input, ctx) => {
     const created = await db.membership.create({ data: input })
 
-    // Delete the session of the updated user so she is forced to log in again to update her membership
-    await db.session.deleteMany({ where: { userId: created.userId } })
+    // After changing the roles, we need to update the session
+    // In other cases we delete the session but for the admin UI that
+    // is a poor choise because we will remove our own session when chaning
+    // or own membership.
+    // We worked around the same issue in the project/new flow by disallowing self manager.
+    const user = await db.user.findFirstOrThrow({
+      where: { id: created.userId },
+      select: selectUserFieldsForSession,
+    })
+    await ctx.session.$setPublicData(user)
 
     return created
   },
