f2363833a057bfe507be4c444cc30b2eb4071630
Tobias <tobias@fixmycity.de>
2024-11-26 15:57:59 +0100

    Invites: Fix email lookup upper and lower case missmatch

--- src/server/auth/shared/getInvite.ts
+++ src/server/auth/shared/getInvite.ts
@@ -6,7 +6,7 @@ export const getInvite = async (inviteToken: string | undefined | null, email: s
 
   // Check if invite is valid and update/invalidate it at the same time
   const invite = await db.invite.findUniqueOrThrow({
-    where: { token: inviteToken, email, status: "PENDING" },
+    where: { token: inviteToken, email: email.toLocaleLowerCase(), status: "PENDING" },
   })
 
   // Explode when someone uses an broken or outdated inviteToken or a non-matching email-address
