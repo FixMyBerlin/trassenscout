9165c384f150a00b1714999258f3df04abc2176e
Tobias <tobias@fixmycity.de>
2024-12-12 11:38:57 +0100

    Pull DB: Update after server migration
    - Use dbmaster locally same as staging,production
    - Change workflow to use ssh tunnel (which requires the `host.docker.internal`)
    - Change script to remove "alias"ing

--- db/remote/restore-dump.sh
+++ db/remote/restore-dump.sh
@@ -5,8 +5,8 @@ set -e
 DIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 ls $DIR/data/dump.sql > /dev/null
 
-alias psql="docker exec -i rsv-builder-db psql -U postgres"
-psql template1 < $DIR/sql/pre-restore.sql
-psql postgres < $DIR/data/dump.sql
-psql postgres < $DIR/sql/post-restore.sql
+docker exec -i ts-db psql -U postgres template1 < $DIR/sql/pre-restore.sql
+docker exec -i ts-db psql -U postgres dbmaster < $DIR/data/dump.sql
+docker exec -i ts-db psql -U postgres dbmaster < $DIR/sql/post-restore.sql
+
 blitz prisma migrate deploy
