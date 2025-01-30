80d4567b4350774c0717c792055e810e3ee4e72f
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2025-01-30 17:07:33 +0100

    Project: replace enableExport checkbox

--- src/app/admin/projects/[projectSlug]/subsections/_components/AdminEnableProjectExportApi.tsx
+++ src/app/admin/projects/[projectSlug]/subsections/_components/AdminEnableProjectExportApi.tsx
@@ -1,7 +1,8 @@
 "use client"
+import { blueButtonStyles } from "@/src/core/components/links"
 import updateProjectExportApi from "@/src/server/projects/mutations/updateProjectExportApi"
 import { useMutation } from "@blitzjs/rpc"
-import React, { useState } from "react"
+import { useState } from "react"
 
 type Props = {
   slug: string
@@ -9,31 +10,25 @@ type Props = {
 }
 
 export const AdminEnableProjectExportApi = ({ slug, exportEnabled }: Props) => {
-  const [exportEnabledEnabled, setExportEnabledEnabled] = useState(exportEnabled)
+  const [isExportEnabled, setIsExportEnabled] = useState(exportEnabled)
   const [createProjectMutation] = useMutation(updateProjectExportApi)
 
-  const handleCheckboxChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const isChecked = event.target.checked
-    setExportEnabledEnabled(isChecked)
-
+  const handleEnableExportClick = async () => {
+    const newExportEnabledState = !isExportEnabled
+    setIsExportEnabled(newExportEnabledState)
     try {
-      await createProjectMutation({ exportEnabled: isChecked, projectSlug: slug })
+      await createProjectMutation({ exportEnabled: newExportEnabledState, projectSlug: slug })
     } catch (error: any) {
-      console.error("Error updating project / enable export API:", error)
+      console.error("Error updating project / enable export: ", error)
+      setIsExportEnabled(!newExportEnabledState)
     }
   }
 
   return (
     <div className="my-3">
-      <label className="block cursor-pointer whitespace-nowrap text-sm font-medium text-gray-700 hover:text-gray-900">
-        <input
-          className="mr-3 h-4 w-4 cursor-pointer rounded border-gray-300 text-blue-600 focus:ring-blue-500"
-          type="checkbox"
-          checked={exportEnabledEnabled}
-          onChange={handleCheckboxChange}
-        />
-        Export-API aktiv
-      </label>
+      <button className={blueButtonStyles} onClick={handleEnableExportClick}>
+        Export {isExportEnabled ? "ausschalten" : "einschalten"}
+      </button>
     </div>
   )
 }
