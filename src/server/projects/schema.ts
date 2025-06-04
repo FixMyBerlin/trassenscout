192a0d859ad0cf4016d97f47b1e27150a70b44d8
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2025-06-04 15:01:09 +0200

    Project: limit number of characters in slug

--- src/server/projects/schema.ts
+++ src/server/projects/schema.ts
@@ -1,8 +1,16 @@
-import { InputNumberOrNullSchema, SlugSchema } from "@/src/core/utils"
+import { InputNumberOrNullSchema } from "@/src/core/utils"
 import { z } from "zod"
 
+const ProjectSlugSchema = z
+  .string()
+  .min(1, { message: "Pflichtfeld. Mindestens 1 Zeichen." })
+  .max(15, { message: "Pflichtfeld. Maximal 15 Zeichen." })
+  .regex(/^[a-z0-9-.]*$/, {
+    message: "Pflichtfeld. Erlaubte Zeichen a-z, 0-9, - (Minus), . (Punkt)",
+  })
+
 export const ProjectSchema = z.object({
-  slug: SlugSchema,
+  slug: ProjectSlugSchema,
   subTitle: z.string().nullish(),
   description: z.string().nullish(),
   logoSrc: z.string().nullish(),
