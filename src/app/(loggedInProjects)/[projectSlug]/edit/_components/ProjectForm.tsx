36adc5d25e43580aca28d14cf4f7389e24a420b2
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2025-06-04 15:01:43 +0200

    Project: update form

--- src/app/(loggedInProjects)/[projectSlug]/edit/_components/ProjectForm.tsx
+++ src/app/(loggedInProjects)/[projectSlug]/edit/_components/ProjectForm.tsx
@@ -7,8 +7,6 @@ import {
   LabeledTextareaField,
   LabeledTextField,
 } from "@/src/core/components/forms"
-import { Link } from "@/src/core/components/links/Link"
-import { quote } from "@/src/core/components/text"
 import {
   getUserSelectOptions,
   UserSelectOptions,
@@ -23,20 +21,18 @@ export const ProjectForm = ({ users, ...props }: Props) => {
       <LabeledTextField
         type="text"
         name="slug"
-        label="Kurz-Titel und URL-Teil"
-        help={`Empfohlenes Format: ${quote(
-          "rs99",
-        )}. Primäre Auszeichnung der Trasse. Wird immer in Großschreibung angezeigt aber in Kleinschreibung editiert. Nachträgliche Änderungen sorgen dafür, dass bisherige URLs (Bookmarks, in E-Mails) nicht mehr funktionieren.`}
+        label="Kurztitel"
+        help="Nachträgliche Änderungen sorgen dafür, dass bisherige URLs (Bookmarks, in E-Mails) nicht mehr funktionieren."
       />
       <LabeledTextField type="text" name="subTitle" label="Untertitel" optional />
-      <LabeledTextField
+      {/* <LabeledTextField
         optional
         type="text"
         name="logoSrc"
         label="Logo"
         placeholder="beispiel.png"
-      />
-      <p className="!mt-1 text-sm text-gray-500">
+      /> */}
+      {/* <p className="!mt-1 text-sm text-gray-500">
         Das Logo wird von FixMyCity{" "}
         <Link
           href="https://s3.console.aws.amazon.com/s3/buckets/trassenscout-public?prefix=assets/&region=eu-central-1"
@@ -45,9 +41,9 @@ export const ProjectForm = ({ users, ...props }: Props) => {
           extern in AWS abgelegt
         </Link>
         , von NextJS intern optimiert und hier referenziert.
-      </p>
+      </p> */}
       <LabeledTextareaField name="description" label="Beschreibung (Markdown)" optional />
-      <LabeledTextareaField
+      {/* <LabeledTextareaField
         optional
         name="partnerLogoSrcs"
         label="Partner-Logos (ein Logo pro Zeile)"
@@ -62,7 +58,7 @@ export const ProjectForm = ({ users, ...props }: Props) => {
           extern in AWS abgelegt
         </Link>
         , von NextJS intern optimiert und hier referenziert.
-      </p>
+      </p> */}
       <LabeledSelect
         name="managerId"
         label="Projektleiter:in"
