3e54c6e0c07a1b07e379d833ead8930231640c6d
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2025-06-26 10:41:12 +0200

    Navigation: make ProjectsSwitch scrollable

--- src/app/_components/layouts/navigation/NavigationLoggedIn/ProjectsSwitch.tsx
+++ src/app/_components/layouts/navigation/NavigationLoggedIn/ProjectsSwitch.tsx
@@ -55,7 +55,7 @@ export const ProjectsSwitch = ({ projects }: Props) => {
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-              <MenuItems className="absolute left-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <MenuItems className="absolute left-0 z-10 mt-2 max-h-[60vh] w-56 origin-top-right divide-y divide-gray-100 overflow-y-auto rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div className="p-1.5">
                   <MenuItem key="dashboard">
                     {({ focus }) => (
