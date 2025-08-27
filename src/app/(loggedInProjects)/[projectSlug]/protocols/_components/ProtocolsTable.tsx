19d6f93e742798af4a8bb4cc03c8f6a112c75de4
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2025-08-27 16:28:57 +0200

    Protocol: add ZeroCase component for empty state

--- src/app/(loggedInProjects)/[projectSlug]/protocols/_components/ProtocolsTable.tsx
+++ src/app/(loggedInProjects)/[projectSlug]/protocols/_components/ProtocolsTable.tsx
@@ -8,6 +8,7 @@ import { ButtonWrapper } from "@/src/core/components/links/ButtonWrapper"
 import { Markdown } from "@/src/core/components/Markdown/Markdown"
 import { TableWrapper } from "@/src/core/components/Table/TableWrapper"
 import { shortTitle } from "@/src/core/components/text"
+import { ZeroCase } from "@/src/core/components/text/ZeroCase"
 import { useProjectSlug } from "@/src/core/routes/useProjectSlug"
 import { IfUserCanEdit } from "@/src/pagesComponents/memberships/IfUserCan"
 
@@ -76,81 +77,89 @@ export const ProtocolsTable = ({
             </div>
           </div>
           <div className="divide-y divide-gray-200 bg-white">
-            {protocols.map((protocol) => (
-              <div key={protocol.id}>
-                <Disclosure
-                  classNamePanel="p-3 text-sm flex gap-4 items-start"
-                  classNameButton={clsx(
-                    "text-left hover:bg-gray-50",
-                    highlightId === protocol.id && "bg-green-50",
-                  )}
-                  button={
-                    <div className="grid flex-grow grid-cols-3">
-                      <div className={clsx(spaceClasses, "text-sm font-medium text-gray-900")}>
-                        {protocol.date ? format(new Date(protocol.date), "P", { locale: de }) : "—"}
+            {!!protocols.length ? (
+              protocols.map((protocol) => (
+                <div key={protocol.id}>
+                  <Disclosure
+                    classNamePanel="p-3 text-sm flex gap-4 items-start"
+                    classNameButton={clsx(
+                      "text-left hover:bg-gray-50",
+                      highlightId === protocol.id && "bg-green-50",
+                    )}
+                    button={
+                      <div className="grid flex-grow grid-cols-3">
+                        <div className={clsx(spaceClasses, "text-sm font-medium text-gray-900")}>
+                          {protocol.date
+                            ? format(new Date(protocol.date), "P", { locale: de })
+                            : "—"}
+                        </div>
+                        <div className={clsx(spaceClasses, "text-sm font-semibold text-blue-500")}>
+                          {protocol.title}
+                        </div>
+                        <div className={clsx(spaceClasses, "text-sm text-gray-900")}>
+                          {protocol.subsection ? (
+                            <Link href={`/${projectSlug}/subsections/${protocol.subsection.slug}`}>
+                              {shortTitle(protocol.subsection.slug)}
+                            </Link>
+                          ) : (
+                            "-"
+                          )}
+                        </div>
                       </div>
-                      <div className={clsx(spaceClasses, "text-sm font-semibold text-blue-500")}>
-                        {protocol.title}
+                    }
+                  >
+                    {protocol.body && (
+                      // for some reasons prose modifiers did not work here
+                      <Markdown
+                        className="rounded-md bg-purple-100 p-2 [&_ol]:ml-4 [&_ol]:list-decimal [&_ol]:leading-tight [&_p]:text-base [&_ul]:ml-4 [&_ul]:list-disc [&_ul]:leading-tight"
+                        markdown={protocol.body}
+                      />
+                    )}
+                    {!!protocol.protocolTopics.length && (
+                      <div>
+                        <p className="mb-4 font-medium text-gray-500">Tags</p>
+                        <ul className="list-inside list-none">
+                          {protocol.protocolTopics.map((topic) => (
+                            <li className="whitespace-nowrap" key={topic.id}>
+                              # {topic.title}
+                            </li>
+                          ))}
+                        </ul>
                       </div>
-                      <div className={clsx(spaceClasses, "text-sm text-gray-900")}>
-                        {protocol.subsection ? (
-                          <Link href={`/${projectSlug}/subsections/${protocol.subsection.slug}`}>
-                            {shortTitle(protocol.subsection.slug)}
-                          </Link>
-                        ) : (
-                          "-"
-                        )}
-                      </div>
-                    </div>
-                  }
-                >
-                  {protocol.body && (
-                    // for some reasons prose modifiers did not work here
-                    <Markdown
-                      className="rounded-md bg-purple-100 p-2 [&_ol]:ml-4 [&_ol]:list-decimal [&_ol]:leading-tight [&_p]:text-base [&_ul]:ml-4 [&_ul]:list-disc [&_ul]:leading-tight"
-                      markdown={protocol.body}
-                    />
-                  )}
-                  {!!protocol.protocolTopics.length && (
+                    )}
                     <div>
-                      <p className="mb-4 font-medium text-gray-500">Tags</p>
-                      <ul className="list-inside list-none">
-                        {protocol.protocolTopics.map((topic) => (
-                          <li className="whitespace-nowrap" key={topic.id}>
-                            # {topic.title}
-                          </li>
-                        ))}
-                      </ul>
+                      <ButtonWrapper className="flex flex-col justify-end gap-3">
+                        <IfUserCanEdit>
+                          <Link icon="edit" href={`/${projectSlug}/protocols/${protocol.id}/edit`}>
+                            Bearbeiten
+                          </Link>
+                          <Link href={`/${projectSlug}/protocols/${protocol.id}`}>
+                            Detailansicht
+                          </Link>
+                        </IfUserCanEdit>
+                      </ButtonWrapper>
+                      <SuperAdminBox>
+                        <p className="text-xs">
+                          <span>Erstellt von </span>
+                          <ProtocolTypePill type={protocol.protocolAuthorType} />
+                          {protocol.protocolAuthorType === ProtocolType.USER && protocol.author && (
+                            <span>{getFullname(protocol.author)}</span>
+                          )}
+                        </p>
+                        <p className="mt-2 text-xs">
+                          <span>Zuletzt bearbeitet von </span>
+                          <ProtocolTypePill type={protocol.protocolUpdatedByType} />{" "}
+                          {protocol.protocolUpdatedByType === ProtocolType.USER &&
+                            protocol.updatedBy && <span>{getFullname(protocol.updatedBy)}</span>}
+                        </p>
+                      </SuperAdminBox>
                     </div>
-                  )}
-                  <div>
-                    <ButtonWrapper className="flex flex-col justify-end gap-3">
-                      <IfUserCanEdit>
-                        <Link icon="edit" href={`/${projectSlug}/protocols/${protocol.id}/edit`}>
-                          Bearbeiten
-                        </Link>
-                        <Link href={`/${projectSlug}/protocols/${protocol.id}`}>Detailansicht</Link>
-                      </IfUserCanEdit>
-                    </ButtonWrapper>
-                    <SuperAdminBox>
-                      <p className="text-xs">
-                        <span>Erstellt von </span>
-                        <ProtocolTypePill type={protocol.protocolAuthorType} />
-                        {protocol.protocolAuthorType === ProtocolType.USER && protocol.author && (
-                          <span>{getFullname(protocol.author)}</span>
-                        )}
-                      </p>
-                      <p className="mt-2 text-xs">
-                        <span>Zuletzt bearbeitet von </span>
-                        <ProtocolTypePill type={protocol.protocolUpdatedByType} />{" "}
-                        {protocol.protocolUpdatedByType === ProtocolType.USER &&
-                          protocol.updatedBy && <span>{getFullname(protocol.updatedBy)}</span>}
-                      </p>
-                    </SuperAdminBox>
-                  </div>
-                </Disclosure>
-              </div>
-            ))}
+                  </Disclosure>
+                </div>
+              ))
+            ) : (
+              <ZeroCase visible={protocols.length} name="Protokolle" />
+            )}
           </div>
         </div>
       </TableWrapper>
