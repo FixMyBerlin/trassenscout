f33b7a09d06e93e493f60d15dd3211a155ad57e9
Tobias <tobias@fixmycity.de>
2025-10-22 10:55:56 +0200

    WKT helper: Round coordinates to 8

--- src/pages/api/survey/utils/coordinatesToWkt.ts
+++ src/pages/api/survey/utils/coordinatesToWkt.ts
@@ -1,5 +1,17 @@
 import { detectGeometryType } from "@/src/app/beteiligung/_components/form/map/utils"
 
+const toCommaString = (coords: [number, number][]) => {
+  return coords.map((c) => toStringPair(c)).join(", ")
+}
+
+const toStringPair = (coords: [number, number]) => {
+  return roundCoords(coords).join(" ")
+}
+
+const roundCoords = (coords: [number, number]) => {
+  return [coords[0].toFixed(8), coords[1].toFixed(8)]
+}
+
 export const coordinatesToWkt = (coordinatesString: string) => {
   if (!coordinatesString) return
 
@@ -10,26 +22,26 @@ export const coordinatesToWkt = (coordinatesString: string) => {
     switch (geometryType) {
       case "point": {
         const pointCoordinates = coordinates as [number, number]
-        return `POINT (${pointCoordinates.join(" ")})`
+        return `POINT (${toStringPair(pointCoordinates)})`
       }
 
       case "lineString": {
         const lineStringCoordinates = coordinates as [number, number][]
-        const wktString = lineStringCoordinates.map((coord) => coord.join(" ")).join(", ")
+        const wktString = toCommaString(lineStringCoordinates)
         return `LINESTRING (${wktString})`
       }
 
       case "multiLineString": {
         const multiLineStringCoordinates = coordinates as [number, number][][]
         const wktString = multiLineStringCoordinates
-          .map((lineString) => `(${lineString.map((coord) => coord.join(" ")).join(", ")})`)
+          .map((lineString) => `(${toCommaString(lineString)})`)
           .join(", ")
         return `MULTILINESTRING (${wktString})`
       }
 
       case "polygon": {
         const polygonCoordinates = coordinates as [number, number][]
-        const wktString = polygonCoordinates.map((coord) => coord.join(" ")).join(", ")
+        const wktString = toCommaString(polygonCoordinates)
         return `POLYGON ((${wktString}))`
       }
 
