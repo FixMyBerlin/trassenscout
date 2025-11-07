e2bcb654607da8bf8275394c9232028229258383
Tobias <tobias@fixmycity.de>
2025-11-07 12:02:27 +0100

    CSV: Add link to docs

--- scripts/csv-import-subsubsections/configs/config-1.ts
+++ scripts/csv-import-subsubsections/configs/config-1.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+  // https://docs.google.com/spreadsheets/d/1LTfMUOzgNnrhyrArTlVUTCmfKkYG9cVm6uZ0XyfXI1g/edit?gid=1855115301#gid=1855115301
   spreadsheetId: "1LTfMUOzgNnrhyrArTlVUTCmfKkYG9cVm6uZ0XyfXI1g",
   tableId: "1855115301",
   userId: 4, // Boris
