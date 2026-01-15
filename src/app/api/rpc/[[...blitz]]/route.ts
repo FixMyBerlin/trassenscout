504534b16652b1b169acb2133909909d58a04851
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-01-15 16:02:02 +0100

    Revert "API: migrate blitz rpc route to app dir"
    This reverts commit 2b400c4472df2eebc86fc71ebeec01dd035ddb85.

--- src/app/api/rpc/[[...blitz]]/route.ts
+++
@@ -1,4 +0,0 @@
-import { withBlitzAuth } from "@/src/blitz-server"
-import { rpcAppHandler } from "@blitzjs/rpc"
-
-export const { GET, POST, HEAD } = withBlitzAuth(rpcAppHandler())
---
+++ src/pages/api/rpc/[[...blitz]].ts
@@ -0,0 +1,4 @@
+import { api } from "@/src/blitz-server"
+import { rpcHandler } from "@blitzjs/rpc"
+
+export default api(rpcHandler({ onError: console.log }))
