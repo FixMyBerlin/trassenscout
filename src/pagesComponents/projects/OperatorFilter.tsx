897e3841882783166f9c1f4c0684ee298ff74e44
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-01-26 16:47:24 +0100

    Operator: rename, only show operators with subsections

--- src/pagesComponents/projects/OperatorFilter.tsx
+++ src/pagesComponents/projects/OperatorFilter.tsx
@@ -7,7 +7,7 @@ import { RadioGroup, RadioGroupLabel, RadioGroupOption } from "@headlessui/react
 import { clsx } from "clsx"
 import router from "next/router"
 
-export const OperatorFilterDropdown = () => {
+export const OperatorFilter = () => {
   const params = useRouterQuery()
   const projectSlug = useProjectSlug()
   const [{ operators }] = useQuery(getOperatorsWithCount, { projectSlug })
@@ -47,23 +47,25 @@ export const OperatorFilterDropdown = () => {
               Alle Baulastträger
             </RadioGroupLabel>
           </RadioGroupOption>
-          {operators.map((operator) => (
-            <RadioGroupOption
-              key={operator.id}
-              value={operator.slug}
-              className={({ active, checked }) =>
-                clsx(
-                  active ? "" : "",
-                  checked ? "bg-blue-900 text-white" : "bg-white ring-1 ring-gray-300",
-                  "relative inline-flex items-center rounded-md px-3 py-2 text-sm whitespace-nowrap text-gray-900 ring-0 hover:cursor-pointer hover:ring-gray-600 focus:z-10",
-                )
-              }
-            >
-              <RadioGroupLabel className="font-semibold" as="span">
-                {shortTitle(operator.slug)}
-              </RadioGroupLabel>
-            </RadioGroupOption>
-          ))}
+          {operators
+            .filter((operator) => operator.subsectionCount > 0)
+            .map((operator) => (
+              <RadioGroupOption
+                key={operator.id}
+                value={operator.slug}
+                className={({ active, checked }) =>
+                  clsx(
+                    active ? "" : "",
+                    checked ? "bg-blue-900 text-white" : "bg-white ring-1 ring-gray-300",
+                    "relative inline-flex items-center rounded-md px-3 py-2 text-sm whitespace-nowrap text-gray-900 ring-0 hover:cursor-pointer hover:ring-gray-600 focus:z-10",
+                  )
+                }
+              >
+                <RadioGroupLabel className="font-semibold" as="span">
+                  {shortTitle(operator.slug)}
+                </RadioGroupLabel>
+              </RadioGroupOption>
+            ))}
         </div>
       </RadioGroup>
     </div>
