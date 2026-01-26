def4cce4427801a35f277d5298e217c25fb09f1d
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-01-26 15:40:27 +0100

    Invite: Remove TabsApp component from new page

--- src/app/(loggedInProjects)/[projectSlug]/invites/new/page.tsx
+++ src/app/(loggedInProjects)/[projectSlug]/invites/new/page.tsx
@@ -1,6 +1,5 @@
 import { Link } from "@/src/core/components/links"
 import { PageHeader } from "@/src/core/components/pages/PageHeader"
-import { TabsApp } from "@/src/core/components/Tabs/TabsApp"
 import { seoIndexTitle } from "@/src/core/components/text"
 import { Metadata, Route } from "next"
 import "server-only"
@@ -24,7 +23,6 @@ export default async function NewProjectTeamInvitePage({ params: { projectSlug }
   return (
     <>
       <PageHeader title="Neues Teammitglied einladen" className="mt-12" />
-      <TabsApp tabs={tabs} className="mt-7" />
       <NewInviteForm projectSlug={projectSlug} />
       <p className="mt-5">
         <Link href={`/${projectSlug}/invites` as Route}>Zurück zur Liste der Einladungen</Link>
