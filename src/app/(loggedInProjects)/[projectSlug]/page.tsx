6e3bcd56415e2ff653b0f7fabc16b9bb7cb30499
Tobias (FMC) <tobias@fixmycity.de>
2026-02-17 16:08:34 +0100

    Revert "Bring back metadata after invoke-cleanup"
    This reverts commit 48ceb1a4165a154322d8461280637a0562649d1e.

--- src/app/(loggedInProjects)/[projectSlug]/page.tsx
+++ src/app/(loggedInProjects)/[projectSlug]/page.tsx
@@ -1,22 +1,9 @@
-import { invoke } from "@/src/blitz-server"
-import { seoTitleSlug } from "@/src/core/components/text"
-import getProject from "@/src/server/projects/queries/getProject"
-import "server-only"
 import { ProjectDashboardClient } from "./_components/ProjectDashboardClient"
+import { Metadata } from "next"
 
-export async function generateMetadata({ params }: { params: { projectSlug: string } }) {
-  try {
-    const project = await invoke(getProject, { projectSlug: params.projectSlug })
-    return {
-      title: seoTitleSlug(project.slug),
-      robots: "noindex",
-    }
-  } catch {
-    return {
-      title: "Projekt",
-      robots: "noindex",
-    }
-  }
+export const metadata: Metadata = {
+  title: "Projekt",
+  robots: "noindex",
 }
 
 export default function ProjectDashboardPage() {
