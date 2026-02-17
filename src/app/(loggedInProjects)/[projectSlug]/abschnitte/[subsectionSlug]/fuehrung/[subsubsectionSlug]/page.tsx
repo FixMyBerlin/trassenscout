48ceb1a4165a154322d8461280637a0562649d1e
Tobias (FMC) <tobias@fixmycity.de>
2026-02-17 15:25:00 +0100

    Bring back metadata after invoke-cleanup

--- src/app/(loggedInProjects)/[projectSlug]/abschnitte/[subsectionSlug]/fuehrung/[subsubsectionSlug]/page.tsx
+++ src/app/(loggedInProjects)/[projectSlug]/abschnitte/[subsectionSlug]/fuehrung/[subsubsectionSlug]/page.tsx
@@ -1,9 +1,30 @@
+import { invoke } from "@/src/blitz-server"
+import { seoTitleSlug } from "@/src/core/components/text"
+import getSubsubsection from "@/src/server/subsubsections/queries/getSubsubsection"
+import "server-only"
 import { SubsectionDashboardClient } from "../../_components/SubsectionDashboardClient"
-import { Metadata } from "next"
 
-export const metadata: Metadata = {
-  title: "Eintrag",
-  robots: "noindex",
+export async function generateMetadata({
+  params,
+}: {
+  params: { projectSlug: string; subsectionSlug: string; subsubsectionSlug: string }
+}) {
+  try {
+    const subsubsection = await invoke(getSubsubsection, {
+      projectSlug: params.projectSlug,
+      subsectionSlug: params.subsectionSlug,
+      subsubsectionSlug: params.subsubsectionSlug,
+    })
+    return {
+      title: seoTitleSlug(subsubsection.slug),
+      robots: "noindex",
+    }
+  } catch {
+    return {
+      title: "Eintrag",
+      robots: "noindex",
+    }
+  }
 }
 
 export default function SubsubsectionDashboardPage() {
--- src/app/(loggedInProjects)/[projectSlug]/abschnitte/[subsectionSlug]/page.tsx
+++ src/app/(loggedInProjects)/[projectSlug]/abschnitte/[subsectionSlug]/page.tsx
@@ -1,9 +1,29 @@
+import { invoke } from "@/src/blitz-server"
+import { seoTitleSlug } from "@/src/core/components/text"
+import getSubsection from "@/src/server/subsections/queries/getSubsection"
+import "server-only"
 import { SubsectionDashboardClient } from "./_components/SubsectionDashboardClient"
-import { Metadata } from "next"
 
-export const metadata: Metadata = {
-  title: "Planungsabschnitt",
-  robots: "noindex",
+export async function generateMetadata({
+  params,
+}: {
+  params: { projectSlug: string; subsectionSlug: string }
+}) {
+  try {
+    const subsection = await invoke(getSubsection, {
+      projectSlug: params.projectSlug,
+      subsectionSlug: params.subsectionSlug,
+    })
+    return {
+      title: seoTitleSlug(subsection.slug),
+      robots: "noindex",
+    }
+  } catch {
+    return {
+      title: "Planungsabschnitt",
+      robots: "noindex",
+    }
+  }
 }
 
 export default function SubsectionDashboardPage() {
