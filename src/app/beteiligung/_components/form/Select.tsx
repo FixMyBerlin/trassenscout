60cd17ab95126e4fd80b7c0065a9550f376bf575
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-03-23 17:10:40 +0100

    Public Survey: Add read-only select option and query parameter handling in SurveySelect component

--- src/app/beteiligung/_components/form/Select.tsx
+++ src/app/beteiligung/_components/form/Select.tsx
@@ -2,25 +2,47 @@ import { FieldWithErrorContainer } from "@/src/app/beteiligung/_components/form/
 import { FieldError } from "@/src/app/beteiligung/_components/form/FieldErrror"
 import { formClasses } from "@/src/app/beteiligung/_components/form/styles"
 import { Description, Field, Label, Select } from "@headlessui/react"
+import { useSearchParams } from "next/navigation"
+import { useEffect } from "react"
 import { useFieldContext } from "../../_shared/hooks/form-context"
 
 type SelectProps = {
   description?: string
+  readOnly?: boolean
   required: boolean
   label: string
+  queryId?: string
   placeholder?: string
   options: { key: string; label: string }[]
 }
 
 export const SurveySelect = ({
   description,
+  readOnly,
   label,
   options,
   placeholder,
   required,
+  queryId,
 }: SelectProps) => {
   const field = useFieldContext<string>()
   const hasError = field.state.meta.errors.length > 0
+
+  // if queryId is set, we update the value of the input field with the value of the query parameter
+  // in Survey[surveyName].tsx we update the query parameter to include the [queryId]
+  // here we get the value of [queryId] from the url, display it and set the value of the input field
+  // the queryId is configured in the field of the respective config file
+
+  const searchParams = useSearchParams()
+  useEffect(() => {
+    if (queryId) {
+      const paramValue = searchParams?.get(queryId)
+      if (paramValue) {
+        const isValidOption = options.some((option) => option.key === paramValue)
+        field.setValue(isValidOption ? paramValue : "unknown")
+      }
+    }
+  }, [field, searchParams, queryId])
 
   return (
     <FieldWithErrorContainer hasError={hasError}>
@@ -32,14 +54,15 @@ export const SurveySelect = ({
           <Description className={formClasses.fieldDescription}>{description}</Description>
         </div>
         <Select
+          disabled={readOnly}
           value={field.state.value}
           onChange={(e) => field.handleChange(e.target.value)}
           aria-label={label}
-          className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-xs focus:border-(--survey-primary-color) focus:ring-(--survey-primary-color) focus:outline-hidden sm:text-sm"
+          className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-xs focus:border-(--survey-primary-color) focus:ring-(--survey-primary-color) focus:outline-hidden data-disabled:bg-gray-200 sm:text-sm"
         >
           {placeholder && (
             <option disabled value="">
-              {placeholder || "Bitte auswählen..."}
+              {placeholder}
             </option>
           )}
           {options.map((option) => (
