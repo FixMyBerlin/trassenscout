a2c4407a5e923f4ba2176647f025ad7487716604
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-04-16 12:54:51 +0200

    ProjectRecord: fix layout

--- src/app/(loggedInProjects)/[projectSlug]/project-records/_components/ProjectRecordSummary.tsx
+++ src/app/(loggedInProjects)/[projectSlug]/project-records/_components/ProjectRecordSummary.tsx
@@ -20,11 +20,11 @@ type Props = {
 export const ProjectRecordSummary = ({ projectRecord }: Props) => {
   return (
     <div className="my-6 space-y-6 font-medium">
-      <div className="grid max-w-3xl grid-cols-6 gap-3">
+      <div className="grid max-w-3xl grid-cols-4 gap-3">
         {projectRecord.assignedTo && (
           <>
             <span className="text-gray-500">Zugewiesen: </span>
-            <span className="col-span-5">
+            <span className="col-span-3">
               <Link
                 classNameOverwrites="inline-flex rounded-md text-inherit no-underline hover:opacity-90 focus:outline-hidden focus-visible:ring-2 focus-visible:ring-orange-500/40"
                 href={createProjectRecordFilterUrl(projectRecord.project.slug, {
@@ -40,32 +40,32 @@ export const ProjectRecordSummary = ({ projectRecord }: Props) => {
           </>
         )}
         <span className="text-gray-500">am/bis: </span>
-        <span className="col-span-5">
+        <span className="col-span-3">
           {format(new Date(projectRecord.date!), "P", { locale: de })}
         </span>
 
         <div className="text-gray-500">Abschnitt: </div>
         {projectRecord.subsection ? (
           <Link
-            className="col-span-5 uppercase"
+            className="col-span-3 uppercase"
             href={`/${projectRecord.project.slug}/abschnitte/${projectRecord.subsection.slug}`}
           >
             {projectRecord.subsection.slug}
           </Link>
         ) : (
-          <span className="col-span-5">Keine Angabe</span>
+          <span className="col-span-3">Keine Angabe</span>
         )}
 
         <div className="text-gray-500">Eintrag: </div>
         {projectRecord.subsubsection ? (
           <Link
-            className="col-span-5 uppercase"
+            className="col-span-3 uppercase"
             href={`/${projectRecord.project.slug}/abschnitte/${projectRecord.subsubsection.subsection.slug}/fuehrung/${projectRecord.subsubsection.slug}`}
           >
             {projectRecord.subsubsection.slug}
           </Link>
         ) : (
-          <span className="col-span-5">Keine Angabe</span>
+          <span className="col-span-3">Keine Angabe</span>
         )}
 
         {projectRecord.project.landAcquisitionModuleEnabled && (
@@ -73,7 +73,7 @@ export const ProjectRecordSummary = ({ projectRecord }: Props) => {
             <div className="text-gray-500">Verhandlungsfläche: </div>
             {projectRecord.acquisitionArea ? (
               <Link
-                className="col-span-5"
+                className="col-span-3"
                 href={
                   `${subsubsectionLandAcquisitionRoute(
                     projectRecord.project.slug,
@@ -86,7 +86,7 @@ export const ProjectRecordSummary = ({ projectRecord }: Props) => {
                 {projectRecord.acquisitionArea.parcel.alkisParcelId})
               </Link>
             ) : (
-              <span className="col-span-5">Keine Angabe</span>
+              <span className="col-span-3">Keine Angabe</span>
             )}
           </>
         )}
