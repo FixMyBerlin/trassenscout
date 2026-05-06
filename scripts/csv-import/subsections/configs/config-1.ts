3df2039d586035f5394d77388133ecec77cbb60b
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-05-06 14:04:00 +0200

    Script: Update configs for subsection and subsubsection CSV imports

--- scripts/csv-import/subsections/configs/config-1.ts
+++ scripts/csv-import/subsections/configs/config-1.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  // Set tableId to your sheet’s “Planungsabschnitte” (subsection) tab GID
+  // https://docs.google.com/spreadsheets/d/1Lf8wluQUIKhoPUlwgBp2J6acChsjhoeXUxLROc5UOX4/edit?gid=880455711#gid=880455711
   spreadsheetId: "1Lf8wluQUIKhoPUlwgBp2J6acChsjhoeXUxLROc5UOX4",
   tableId: "880455711",
   userId: 4,
---
+++ scripts/csv-import/subsubsections/configs/config-4.ts
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  // https://docs.google.com/spreadsheets/d/1jpHiUPhgj7EXM7HZ-mA4u2KB51eSedvKmVd0fNynJXM/edit?gid=0#gid=0
+  spreadsheetId: "1jpHiUPhgj7EXM7HZ-mA4u2KB51eSedvKmVd0fNynJXM",
+  tableId: "0",
+  userId: 4, // Boris
+}
