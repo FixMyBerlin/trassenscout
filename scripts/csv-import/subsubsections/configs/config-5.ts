85277e6cee4cdf792cb8ee798c065df17e3b7e26
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-05-13 16:05:37 +0200

    Import: add configs for frm3

---
+++ scripts/csv-import/subsubsections/configs/config-5.ts
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  // https://docs.google.com/spreadsheets/d/1Rin0Y77FN4GDACwf9dURXilU2mY3RqgVyUagjFDXHNM/edit?gid=254776698#gid=254776698
+  spreadsheetId: "1Rin0Y77FN4GDACwf9dURXilU2mY3RqgVyUagjFDXHNM",
+  tableId: "254776698",
+  userId: 4, // Boris
+}
