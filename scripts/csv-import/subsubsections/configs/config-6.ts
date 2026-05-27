9cff5237d0186af1cae3a9f586717896853dd264
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-05-27 13:38:36 +0200

    Import: add configs for frm8

---
+++ scripts/csv-import/subsubsections/configs/config-6.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  // https://docs.google.com/spreadsheets/d/1aTEtjDHvECoCzGyNNzIk8d-CScEGucbqWK-TDeAuA-o/edit?gid=653421111#gid=653421111
+  spreadsheetId: "1aTEtjDHvECoCzGyNNzIk8d-CScEGucbqWK-TDeAuA-o",
+  tableId: "1965679137",
+  userId: 4,
+}
+// bun scripts/csv-import/subsubsections/index.ts config-6 staging
