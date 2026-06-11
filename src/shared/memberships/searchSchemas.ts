632b4c39a179426e05f5499ba641d73f79fc5591
Tobias <tobias@fixmycity.de>
2026-06-11 06:28:58 +0200

    Migration: Fix membership/new

--- src/shared/memberships/searchSchemas.ts
+++ src/shared/memberships/searchSchemas.ts
@@ -1,5 +1,8 @@
 import { z } from "zod"
 
 export const membershipNewSearchSchema = z.object({
-  userId: z.string().optional(),
+  userId: z
+    .union([z.string(), z.number()])
+    .optional()
+    .transform((value) => (value === undefined ? undefined : String(value))),
 })
