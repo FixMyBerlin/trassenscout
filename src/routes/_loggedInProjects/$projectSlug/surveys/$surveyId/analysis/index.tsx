9e5abcc00bd9c5e9289ba5fed3f3f9c12b4c243a
Tobias <tobias@fixmycity.de>
2026-06-25 04:47:49 +0200

    Autoformatting

--- src/routes/_loggedInProjects/$projectSlug/surveys/$surveyId/analysis/index.tsx
+++ src/routes/_loggedInProjects/$projectSlug/surveys/$surveyId/analysis/index.tsx
@@ -4,22 +4,22 @@ import { absoluteTitleHead } from "@/src/routeHead"
 import { surveyQueryOptions } from "@/src/server/surveys/surveysQueryOptions"
 import { surveyTabsQueryOptions } from "@/src/server/surveys/surveyTabsQueryOptions"
 
-export const Route = createFileRoute(
-  "/_loggedInProjects/$projectSlug/surveys/$surveyId/analysis/",
-)({
-  head: () => absoluteTitleHead("Beteiligung"),
-  ssr: true,
-  loader: async ({ context, params }) => {
-    const surveyId = Number(params.surveyId)
-    const [survey] = await Promise.all([
-      context.queryClient.ensureQueryData(
-        surveyQueryOptions({ projectSlug: params.projectSlug, id: surveyId }),
-      ),
-      context.queryClient.ensureQueryData(
-        surveyTabsQueryOptions({ projectSlug: params.projectSlug, surveyId }),
-      ),
-    ])
-    return { survey }
+export const Route = createFileRoute("/_loggedInProjects/$projectSlug/surveys/$surveyId/analysis/")(
+  {
+    head: () => absoluteTitleHead("Beteiligung"),
+    ssr: true,
+    loader: async ({ context, params }) => {
+      const surveyId = Number(params.surveyId)
+      const [survey] = await Promise.all([
+        context.queryClient.ensureQueryData(
+          surveyQueryOptions({ projectSlug: params.projectSlug, id: surveyId }),
+        ),
+        context.queryClient.ensureQueryData(
+          surveyTabsQueryOptions({ projectSlug: params.projectSlug, surveyId }),
+        ),
+      ])
+      return { survey }
+    },
+    component: PageSurveyAnalysis,
   },
-  component: PageSurveyAnalysis,
-})
+)
