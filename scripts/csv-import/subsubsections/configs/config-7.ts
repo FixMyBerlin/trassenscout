3bed2fddc81240916a2f803f15ed7aaab5ee543b
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-07-02 10:31:45 +0200

    Revert "Import: add configs for zielnetz ohv"
    This reverts commit 2019c170f739c15e2bff39df06832fe1078cc466.

--- scripts/csv-import/subsubsections/configs/config-7.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-export default {
-  // https://docs.google.com/spreadsheets/d/1luDRvHVYYhlQH2WV7aP2nQUJKFwo1vjy8MacPUQXfUc/edit?gid=0#gid=0
-  spreadsheetId: "1luDRvHVYYhlQH2WV7aP2nQUJKFwo1vjy8MacPUQXfUc",
-  tableId: "0",
-  userId: 4,
-}
-// bun scripts/csv-import/subsubsections/index.ts config-7 staging
