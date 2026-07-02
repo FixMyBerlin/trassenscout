2cd8b90a8c23f1921cf403467516b27ab6b95710
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-07-02 10:31:45 +0200

    Revert "Subsubsection: remove title in side panel"
    This reverts commit 6404e0aa760a52b7ef51ed572ead335ad875e842.

--- src/app/(loggedInProjects)/[projectSlug]/abschnitte/[subsectionSlug]/_components/SubsubsectionDetailsContent.tsx
+++ src/app/(loggedInProjects)/[projectSlug]/abschnitte/[subsectionSlug]/_components/SubsubsectionDetailsContent.tsx
@@ -107,7 +107,7 @@ export const SubsubsectionDetailsContent = ({ subsubsection, className, header }
 
   return (
     <SubsubsectionPanel
-      title=""
+      title="Allgemeines"
       action={
         <IfUserCanEdit>
           <Link
