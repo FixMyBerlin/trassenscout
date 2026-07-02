1eddaae93064b9b2fd314257204d56ea3edd240b
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-07-02 10:31:41 +0200

    Revert "Add footer link for Demo"
    This reverts commit 4859e03adfc50cebf1a35f403fa553814e200804.

--- src/app/_components/layouts/footer/links.const.ts
+++ src/app/_components/layouts/footer/links.const.ts
@@ -22,11 +22,6 @@ export const fixmyCityLinks: FooterLink<string>[] = [
     blank: true,
   },
   {
-    name: "Demo Termin vereinbaren",
-    href: "https://fixmycity.de/termin-vereinbaren/" as Route<string>,
-    blank: true,
-  },
-  {
     name: "TILDA - Unser Verkehrsplanungs Web-GIS",
     href: "https://tilda-geo.de" as Route<string>,
     blank: true,
