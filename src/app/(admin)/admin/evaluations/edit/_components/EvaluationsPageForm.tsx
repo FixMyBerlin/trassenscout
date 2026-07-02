c5e335d04191bc581f31f649bdeab6d7966d4951
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2026-07-02 10:31:44 +0200

    Revert "eval page"
    This reverts commit 5292c59c4c85dcebf49ff144a8ea3b3b30992875.

--- src/app/(admin)/admin/evaluations/edit/_components/EvaluationsPageForm.tsx
+++ src/app/(admin)/admin/evaluations/edit/_components/EvaluationsPageForm.tsx
@@ -1,9 +1,4 @@
-import {
-  Form,
-  FormProps,
-  LabeledTextareaField,
-  LabeledTextField,
-} from "@/src/core/components/forms"
+import { Form, FormProps, LabeledTextareaField, LabeledTextField } from "@/src/core/components/forms"
 import { z } from "zod"
 
 type Props = FormProps<z.ZodType<any, any>>
